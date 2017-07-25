766ec791e7c94aaeb103a187f69114129ce62708
Tim Needham <tim.needham@wmfs.net>
2017-07-25 17:48:36 +0100

    Adds runner support for $FLOBOT_ADMIN_USERID and $FLOBOT_ADMIN_USERID

--- flobot-runner/README.md
+++ flobot-runner/README.md
@@ -7,10 +7,17 @@ $ npm test
 ```
 
 ## Environment Variables
-> Set up these environment variables for use within flobot-runner:
-> PG_CONNECTION_STRING
-> FLOBOT_BLUEPRINTS_PATH
-> FLOBOT_PLUGINS_PATH
+
+| Variable Name          | Description |
+| ---------------------- | ----------- |
+| PG_CONNECTION_STRING   |  .          | 
+| FLOBOT_AUTH_SECRET     |  .          |
+| FLOBOT_AUTH_AUDIENCE   |  .          |
+| FLOBOT_PLUGINS_PATH    |  .          |
+| FLOBOT_BLUEPRINTS_PATH |  .          |
+| FLOBOT_ADMIN_USERID    |  .          |
+| FLOBOT_ADMIN_ROLES     |  .          |
+
 
 ## <a name="license"></a>License
 [MIT](https://github.com/wmfs/flobot-runner/blob/master/LICENSE.md)
--- flobot-runner/lib/index.js
+++ flobot-runner/lib/index.js
@@ -5,6 +5,17 @@ const process = require('process')
 const jwt = require('jsonwebtoken')
 const Buffer = require('safe-buffer').Buffer
 
+// Add an admin user if defined via $FLOBOT_ADMIN_USERID
+let adminUserId = process.env.FLOBOT_ADMIN_USERID
+if (adminUserId) {
+  // UserID might be double-quoted, so remove if that's the case
+  if (adminUserId[0] === '"' && adminUserId[adminUserId.length - 1] === '"') {
+    adminUserId = adminUserId.substring(1, adminUserId.length - 1)
+  }
+  config.config.defaultUsers = {}
+  config.config.defaultUsers[adminUserId] = process.env.FLOBOT_ADMIN_ROLES.split(',')
+}
+
 flobot.boot(
   config,
   function (err, services) {
@@ -29,7 +40,6 @@ flobot.boot(
           console.log(`Server listening on port ${config.config.serverPort}!\n`)
           services.rbac.rbac.debug()
           console.log(`Admin token: ${adminToken}`)
-          console.log(config)
         })
       }
 
