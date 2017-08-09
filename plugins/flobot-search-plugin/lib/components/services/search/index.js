7570b6136999a7ad662367fd9578da87574e474b
Meena Hoda <meena.hoda@wmfs.net>
2017-08-09 12:12:10 +0100

    Created a skeleton plugin 'flobot-search-plugin' with service set up

---
+++ plugins/flobot-search-plugin/lib/components/services/search/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const schema = require('./schema.json')
+
+class SearchService {
+  boot (options, callback) {
+    console.log('INIT SOLR')
+    let client = options.bootedServices.storage.client
+    // console.log(client)
+    callback(null)
+  }
+}
+
+module.exports = {
+  serviceClass: SearchService,
+  bootAfter: ['storage'],
+  schema: schema
+}
