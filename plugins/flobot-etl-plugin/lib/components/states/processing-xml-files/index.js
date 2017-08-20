586471170445bd114ee57696b2dbe7f017450644
Tim Needham <tim.needham@wmfs.net>
2017-08-20 22:06:33 +0100

    Beginnings of processingXmlFiles state

---
+++ plugins/flobot-etl-plugin/lib/components/states/processing-xml-files/index.js
@@ -0,0 +1,25 @@
+'use strict'.gitignore
+const schema = require('./schema.json')
+const debug = require('debug')('processingCsvFiles')
+
+class ProcessingXmlFiles {
+  init (stateConfig, options, callback) {
+    callback(null)
+  }
+
+  enter (flobot, data, callback) {
+    debug(`Flobot ${flobot.flobotId} is entering state 'processingXmlFiles`)
+    callback(null)
+  }
+
+  leave (flobot, data, callback) {
+    debug(`Flobot ${flobot.flobotId} is leaving state 'processingXmlFiles'`)
+    callback(null)
+  }
+}
+
+module.exports = {
+  autoNudge: true,
+  stateClass: ProcessingXmlFiles,
+  schema: schema
+}
