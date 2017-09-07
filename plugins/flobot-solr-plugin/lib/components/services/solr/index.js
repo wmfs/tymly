67d876d287cd8308f3d8db77afa8c9967ce3ad1e
Jasvinder Bahra <jas.bahra@wmfs.net>
2017-09-07 14:09:58 +0100

    Initial test

--- plugins/flobot-solr-plugin/lib/components/services/solr/index.js
+++ plugins/flobot-solr-plugin/lib/components/services/solr/index.js
@@ -2,7 +2,13 @@
 
 class SolrService {
   boot (options, callback) {
+    console.log('Solr Service says Hello World!')
+    console.log(options)
     callback(null)
+  }
+
+  generateSelect (model, attribute) {
+    return 'hello world!'
   }
 }
 
--- plugins/flobot-solr-plugin/test/index.js
+++ plugins/flobot-solr-plugin/test/index.js
@@ -1,9 +1,11 @@
 /* eslint-env mocha */
+'use strict'
 
 const expect = require('chai').expect
 const flobot = require('flobot')
 const path = require('path')
 // const debug = require('debug')('flobot-solr-plugin')
+const SolrService = require('./../lib/components/services/solr/index.js').serviceClass
 
 describe('Simple solr tests', function () {
   let solrService
@@ -13,10 +15,8 @@ describe('Simple solr tests', function () {
         pluginPaths: [
           path.resolve(__dirname, './../lib')
         ],
-        blueprintPaths: [
-        ],
-        config: {
-        }
+        blueprintPaths: [],
+        config: {}
       },
       function (err, flobotServices) {
         expect(err).to.eql(null)
@@ -25,5 +25,43 @@ describe('Simple solr tests', function () {
         done()
       }
     )
+  })
+
+  it('generate a SQL CREATE VIEW from a model and an attribute', () => {
+    const plugin = new SolrService()
+    const model = {
+      'title': 'address',
+      'description': '...',
+      'primaryKey': ['uprn'],
+      'type': 'object',
+      'properties': {
+        'uprn': {
+          'type': 'integer',
+          'maxLength': 12,
+          'description': '...'
+        },
+        'streetName': {
+          'type': 'string',
+          'maxLength': 128,
+          'description': '...'
+        },
+        'postCode': {
+          'type': 'string',
+          'maxLength': 10,
+          'description': '...'
+        }
+      },
+      'required': ['uprn']
+    }
+    const attribute = {
+      'modelId': 'address',
+      'attributeMapping': {
+        'address': '@streetName'
+      }
+    }
+
+    const select = plugin.generateSelect(model, attribute)
+
+    expect(select).to.be.a('string')
   })
 })
