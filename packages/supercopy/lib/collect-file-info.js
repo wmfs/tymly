1f4caa75bfb9b42967a2f668aee48e943efcbb3a
Jez Higgins <jez@jezuk.co.uk>
2017-09-12 22:18:43 +0100

    supercopy - factor out common stuff in directory traversal

--- packages/supercopy/lib/collect-file-info.js
+++ packages/supercopy/lib/collect-file-info.js
@@ -1,6 +1,5 @@
 
 const fs = require('fs')
-const async = require('async')
 const path = require('path')
 const getColumnNames = require('./get-column-names')
 const upath = require('upath')
@@ -11,13 +10,13 @@ const readdirAsync = promisify(fs.readdir)
 const lstatAsync = promisify(fs.lstat)
 const columnNamesAsync = promisify(getColumnNames)
 
-module.exports = function(options, callback) {
+module.exports = function (options, callback) {
   collect(options)
     .then(info => callback(null, info))
     .catch(err => callback(err, null))
 }
 
-async function collect(options) {
+async function collect (options) {
   const rootDir = options.sourceDir
   debug(`Staring to collect file info for ${rootDir}`)
 
@@ -32,7 +31,7 @@ async function collect(options) {
     for (const [filePath, size] of await filesUnder(dirPath)) {
       debug(`+   ./${path.basename(filePath)}:`)
 
-      const columnNames = await columnNamesAsync(filePath, options);
+      const columnNames = await columnNamesAsync(filePath, options)
       action[upath.normalize(filePath)] = {
         tableName: path.basename(filePath, path.extname(filePath)),
         columnNames: columnNames,
@@ -44,30 +43,32 @@ async function collect(options) {
   return info
 } // collect
 
-async function directoriesUnder(rootDir) {
+async function directoriesUnder (rootDir) {
+  return directoryContents(
+    rootDir,
+    stats => stats.isDirectory,
+    (path, stats) => path
+  )
+} // directoriesUnder
+
+async function filesUnder (rootDir) {
+  return directoryContents(
+    rootDir,
+    stats => (stats.isFile() && stats.size !== 0),
+    (path, stats) => [path, stats.size]
+  )
+} // filesUnder
+
+async function directoryContents (rootDir, filter, transform) {
   const items = await readdirAsync(rootDir)
   debug(`Directory contents [${items}]`)
   const statedItems = await Promise.all(
     items.map(async item => {
-      const dirPath = path.join(rootDir, item)
-      const stats = await lstatAsync(dirPath)
-      return stats.isDirectory() ? dirPath : null
+      const itemPath = path.join(rootDir, item)
+      const stats = await lstatAsync(itemPath)
+      return filter(stats) ? transform(itemPath, stats) : null
     })
   )
   const dirs = statedItems.filter(d => !!d)
   return dirs
-} // directoriesUnder
-
-async function filesUnder(rootDir) {
-  const items = await readdirAsync(rootDir)
-  const statedItems = await Promise.all(
-    items.map(async item => {
-      const filePath = path.join(rootDir, item)
-      const stats = await lstatAsync(filePath)
-      return (stats.isFile() && stats.size !== 0) ?
-        [filePath, stats.size] : null
-    })
-  )
-  const files = statedItems.filter(f => !!f)
-  return files
-} // filesUnder
+} // directoryContents
