58b0a27a78dc08040354e72e92a81c0a53b031e5
Jez Higgins <jez@jezuk.co.uk>
2017-09-12 15:19:05 +0100

    supercopy - outer lstat to lstatAsync

--- packages/supercopy/lib/collect-file-info.js
+++ packages/supercopy/lib/collect-file-info.js
@@ -25,58 +25,53 @@ module.exports = function (options, callback) {
         items,
         function (item, cb) {
           const dirPath = path.join(rootDir, item)
-          fs.lstat(
-            dirPath,
-            function (err, stats) {
-              if (err) {
-                cb(err)
-              } else {
-                if (stats.isDirectory()) {
-                  debug(`+ ./${item}:`)
-                  const action = {}
-                  info[item] = action
+          lstatAsync(dirPath)
+            .then(stats => {
+              if (stats.isDirectory()) {
+                debug(`+ ./${item}:`)
+                const action = {}
+                info[item] = action
 
-                  readdirAsync(dirPath)
-                    .then(actionItems => {
-                      async.eachSeries(
-                        actionItems,
-                        function (actionItem, cb2) {
-                          const filePath = path.join(dirPath, actionItem)
+                readdirAsync(dirPath)
+                  .then(actionItems => {
+                    async.eachSeries(
+                      actionItems,
+                      function (actionItem, cb2) {
+                        const filePath = path.join(dirPath, actionItem)
 
-                          lstatAsync(filePath)
-                            .then(fileStats => {
-                              if (!fileStats.isFile() || fileStats.size === 0)
-                                return cb2();
+                        lstatAsync(filePath)
+                          .then(fileStats => {
+                            if (!fileStats.isFile() || fileStats.size === 0)
+                              return cb2();
 
-                              columnNamesAsync(filePath, options)
-                                .then(columnNames => {
-                                  action[upath.normalize(filePath)] = {
-                                    tableName: path.basename(actionItem, path.extname(actionItem)),
-                                    columnNames: columnNames,
-                                    size: fileStats.size
-                                  }
-                                  cb2()
-                                })
-                                .catch(err => cb2(err));
-                            })
-                            .catch(err => cb2(err))
-                        },
-                        function (err) {
-                          if (err) {
-                            cb(err)
-                          } else {
-                            cb()
-                          }
+                            columnNamesAsync(filePath, options)
+                              .then(columnNames => {
+                                action[upath.normalize(filePath)] = {
+                                  tableName: path.basename(actionItem, path.extname(actionItem)),
+                                  columnNames: columnNames,
+                                  size: fileStats.size
+                                }
+                                cb2()
+                              })
+                              .catch(err => cb2(err)) // columnNamesAsync
+                          })
+                          .catch(err => cb2(err)) // lstatAsync
+                      },
+                      function (err) {
+                        if (err) {
+                          cb(err)
+                        } else {
+                          cb()
                         }
-                      )
-                    })
-                    .catch(err => cb(err));
-                } else {
-                  cb()
-                }
+                      }
+                    )
+                  })
+                  .catch(err => cb(err)) // readdirAsync
+              } else {
+                cb()
               }
-            }
-          )
+            })
+            .catch(err => cb(err)) // lstatAsync
         },
         function (err) {
           if (err) {
