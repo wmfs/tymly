87e0c3b7a17888f0932bb3069905749ee7062dd8
Jasvinder Bahra <jas.bahra@wmfs.net>
2017-09-13 18:17:56 +0100

    Implementation of functions to instruct solr to do full and delta imports

--- plugins/flobot-solr-plugin/lib/components/services/solr/index.js
+++ plugins/flobot-solr-plugin/lib/components/services/solr/index.js
@@ -1,29 +1,40 @@
 'use strict'
 
 const _ = require('lodash')
-// const debug = require('debug')('flobot-solr-plugin')
+const debug = require('debug')('flobot-solr-plugin')
 const process = require('process')
+const boom = require('boom')
+const request = require('request')
+const defaultSolrIndexFields = require('./solr-index-fields.json')
 
 class SolrService {
   boot (options, callback) {
     this.viewNamespace = 'fbot'
-
-    const solrUrl = process.env.SOLR_URL
-    options.messages.info(`Using Solr... (${solrUrl})`)
+    this.solrUrl = process.env.SOLR_URL === undefined ? 'http://localhost:8983/solr' : process.env.SOLR_URL
+    options.messages.info(`Using Solr... (${this.solrUrl})`)
 
     if (!options.blueprintComponents.hasOwnProperty('searchDocs')) {
       this.fields = []
       this.createViewSQL = null
       callback(null)
+    } else {
+      this.client = options.bootedServices.storage.client
+
+      if (!this.client) {
+        callback(boom.notFound('failed to boot solr service: no database client available'))
+      } else {
+        if (options.config.solrIndexFields === undefined) {
+          this.fields = this.constructFieldArray(defaultSolrIndexFields)
+        } else {
+          this.fields = this.constructFieldArray(options.config.solrIndexFields)
+        }
+        this.createViewSQL = this.buildCreateViewStatement(
+          this.constructModelArray(options.blueprintComponents.models),
+          this.constructSearchAttributeArray(options.blueprintComponents.searchDocs))
+
+        this.client.query(this.createViewSQL, [], callback)
+      }
     }
-
-    this.client = options.bootedServices.storage.client
-    this.fields = this.constructFieldArray(options.config.solrIndexFields)
-    this.createViewSQL = this.buildCreateViewStatement(
-      this.constructModelArray(options.blueprintComponents.models),
-      this.constructSearchAttributeArray(options.blueprintComponents.searchDocs))
-
-    this.client.query(this.createViewSQL, [], callback)
   }
 
   constructModelArray (models) {
@@ -87,6 +98,64 @@ class SolrService {
     }
 
     return `CREATE OR REPLACE VIEW ${this.viewNamespace}.solr_data AS \n${selects.join('\nUNION\n')};`
+  }
+
+  executeSolrFullReindex (core, cb) {
+    const uniqueIdentifier = new Date().getTime()
+    const url = `${this.solrUrl}/${core}/dataimport?_=${uniqueIdentifier}&indent=on&wt=json`
+
+    request.post(
+      {
+        url: url,
+        form: {
+          'clean': true,
+          'command': 'full-import',
+          'commit': true,
+          'core': core,
+          'name': 'dataimport',
+          'optimize': false,
+          'verbose': false
+        }
+      },
+      function (err, response, body) {
+        if (err) {
+          debug('solr dataimport command failed', err)
+          cb(err)
+        } else {
+          debug(body)
+          cb(null)
+        }
+      }
+    )
+  }
+
+  executeSolrDeltaReindex (core, cb) {
+    const uniqueIdentifier = new Date().getTime()
+    const url = `${this.solrUrl}/${core}/dataimport?_=${uniqueIdentifier}&indent=on&wt=json`
+
+    request.post(
+      {
+        url: url,
+        form: {
+          'clean': true,
+          'command': 'delta-import',
+          'commit': true,
+          'core': core,
+          'name': 'dataimport',
+          'optimize': false,
+          'verbose': false
+        }
+      },
+      function (err, response, body) {
+        if (err) {
+          debug('solr dataimport command failed', err)
+          cb(err)
+        } else {
+          debug(body)
+          cb(null)
+        }
+      }
+    )
   }
 }
 
