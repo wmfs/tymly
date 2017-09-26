580aacb2d35cb780792b24250954a7fb0f482624
Meena Hoda <meena.hoda@wmfs.net>
2017-09-26 14:17:31 +0100

    Added tests for the statebox refactor

---
+++ blueprints/irs-blueprint/test/test.js
@@ -0,0 +1,50 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const flobot = require('flobot')
+const path = require('path')
+const expect = require('chai').expect
+const STATE_MACHINE_NAME = 'wmfs_incidentEditor_1_0'
+
+describe('data import', function () {
+  this.timeout(5000)
+  let statebox
+
+  it('should startup flobot', function (done) {
+    flobot.boot(
+      {
+        pluginPaths: [
+          require.resolve('flobot-forms-plugin'),
+          require.resolve('flobot')
+        ],
+        blueprintPaths: [
+          path.resolve(__dirname, './../')
+        ],
+        config: {}
+      },
+      function (err, flobotServices) {
+        expect(err).to.eql(null)
+        statebox = flobotServices.statebox
+
+        done()
+      }
+    )
+  })
+
+  it('should perform the state machine', function (done) {
+    statebox.startExecution(
+      {},  // input
+      STATE_MACHINE_NAME, // state machine name
+      {
+        sendResponse: 'COMPLETE'
+      }, // options
+      function (err, executionDescription) {
+        expect(err).to.eql(null)
+        expect(executionDescription.status).to.eql('SUCCEEDED')
+        expect(executionDescription.currentStateName).to.eql('ImportingCsvFiles')
+        done()
+      }
+    )
+  })
+})
