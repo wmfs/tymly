865c536898d93f4e0d6f43768992aafcf439de65
Reece Brend <reece.brend@wmfs.net>
2017-09-27 11:18:13 +0100

    Adds test.js to building-blueprint

---
+++ blueprints/building-blueprint/test/test.js
@@ -0,0 +1,53 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const flobot = require('flobot')
+const path = require('path')
+const expect = require('chai').expect
+const STATE_MACHINE_NAME = 'wmfs_refreshFromCsvFile_1_0'
+const Statebox = require('statebox')
+
+describe('data import', function () {
+  this.timeout(5000)
+
+  let statebox = new Statebox()
+
+  it('should startup flobot', function (done) {
+    flobot.boot(
+      {
+        pluginPaths: [
+          require.resolve('flobot-pg-plugin')
+        ],
+        blueprintPaths: [
+          path.resolve(__dirname, './../')
+        ],
+        config: {}
+      },
+      function (err, flobotServices) {
+        expect(err).to.eql(null)
+        statebox = flobotServices.statebox
+
+        done()
+      }
+    )
+  })
+
+  it('should create and populate the wmfs.building database table', function (done) {
+    statebox.startExecution(
+      {
+        sourceDir: path.resolve(__dirname, './fixtures/input')
+      },  // input
+      STATE_MACHINE_NAME, // state machine name
+      {
+        sendResponse: 'COMPLETE'
+      }, // options
+      function (err, executionDescription) {
+        expect(err).to.eql(null)
+        expect(executionDescription.status).to.eql('SUCCEEDED')
+        expect(executionDescription.currentStateName).to.eql('ImportingCsvFiles')
+        done()
+      }
+    )
+  })
+})
