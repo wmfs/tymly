2a5f6c7b66df6dd252c2f27752355fceb76a5b98
Jez Higgins <jez@jezuk.co.uk>
2017-10-02 14:53:07 +0100

    pull package names and version numbers from package.jsons

---
+++ tools/tymly-packager/lib/read_version_numbers.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const fs = require('fs')
+
+function readVersionNumber(pkg) {
+  const jsonFile = path.join(pkg, 'package.json')
+  const json = JSON.parse(fs.readFileSync(jsonFile))
+  return [ json.name, json.version ]
+} // readVersionNumbers
+
+function readVersionNumbers(searchRoot, packages) {
+  const versions = { }
+
+  for (const pkg of packages) {
+    const [name, version] = readVersionNumber(path.join(searchRoot, pkg))
+
+    versions[name] = version
+  } // for ...
+
+  return versions
+} // readVersionNumbers
+
+module.exports = readVersionNumbers
--- tools/tymly-packager/test/test.js
+++ tools/tymly-packager/test/test.js
@@ -3,6 +3,7 @@ const expect = require('chai').expect
 const path = require('path')
 
 const gatherPackages = require('../lib/gather_packages.js')
+const readVersionNumbers = require('../lib/read_version_numbers.js')
 
 const searchRoot = path.resolve(__dirname, './fixtures/packages')
 
@@ -21,3 +22,20 @@ describe('Package gathering', () => {
     }) // it ...
   } // for ...
 }) // describe ...
+
+describe('Generate manifest version numbers', () => {
+  const tests = [
+    ['one top level package', ['simple-package'], {'simple-package': '1.0.0'}],
+    ['package with node_modules', ['package-with-node-modules'], {'package-with-node-modules': '0.0.0'}],
+    ['nested including a directory to ignore', ['nested-directory/package-1', 'nested-directory/package-2'], {'package-A': '1.0.a', 'package-B': '1.0.b'}]
+  ]
+
+  for (const [ label, packages, results ] of tests) {
+    it(label, async () => {
+      const versions = await readVersionNumbers(searchRoot, packages)
+
+      expect(versions).to.deep.equal(results)
+    }) // it ...
+  } // for ...
+
+}) // describe
