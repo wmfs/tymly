68e891007b6f5b8709d0154bcc569ddcba2c0777
Jez Higgins <jez@jezuk.co.uk>
2017-10-05 10:33:59 +0100

    standard fixes

--- tools/tymly-packager/lib/bundler.js
+++ tools/tymly-packager/lib/bundler.js
@@ -5,18 +5,18 @@ const buildBundle = require('./build_bundle.js')
 const fs = require('fs')
 const path = require('path')
 
-function packageDetails(dir) {
+function packageDetails (dir) {
   console.log(`Bundling ${dir} ...`)
   const packages = readVersionNumbers(dir, gatherPackages(dir))
   console.log(`... found ${packages.length} packages`)
   return packages
 } // packageDetails
 
-function cleanUpTarballs(dir, tarballs) {
+function cleanUpTarballs (dir, tarballs) {
   tarballs.forEach(t => fs.unlinkSync(path.join(dir, t)))
 } // cleanUpTarballs
 
-async function bundleForDeploy(dir) {
+async function bundleForDeploy (dir) {
   const packages = packageDetails(dir)
   const tarballs = await packPackages(dir, packages)
   await buildBundle(dir, packages, tarballs)
