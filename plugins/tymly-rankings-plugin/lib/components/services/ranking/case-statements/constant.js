2b89557854376a802ec44432f982acecf7b24f04
Liliana Judge <liliana.judge@wmfs.net>
2017-10-06 11:39:36 +0100

    deleted ranking dir

--- plugins/tymly-rankings-plugin/lib/components/services/ranking/case-statements/constant.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const _ = require('lodash')
-
-module.exports = function constant (factorName, factorObj, schema, table, column) {
-  return `${factorObj.value} as ${_.snakeCase(factorName)}_score `
-}
--- plugins/tymly-rankings-plugin/lib/components/services/ranking/generate-join-statement.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = function generateJoinStatement (factorObj, schema, table, columnToMatchOn) {
-  if (factorObj.type !== 'constant') {
-    return `JOIN ${schema}.${table} ${table} ON ${table}.${columnToMatchOn} = g.${columnToMatchOn} `
-  } else {
-    return ``
-  }
-}
