05b781a8624d243fe9a2a3ed026fbf047dea4a08
Meena Hoda <meena.hoda@wmfs.net>
2017-10-09 12:01:50 +0100

    Ranking state-resource now gets the schema from the context

--- plugins/tymly-rankings-plugin/lib/components/state-resources/refresh-ranking/index.js
+++ plugins/tymly-rankings-plugin/lib/components/state-resources/refresh-ranking/index.js
@@ -5,26 +5,23 @@ const generateView = require('./../../services/rankings/generate-view-statement.
 class RefreshRanking {
   init (resourceConfig, env, callback) {
     this.propertyType = resourceConfig.propertyType
-    this.schema = 'test' // this should come from where ever the namespace is located
-
-    let key = this.schema + '_' + this.propertyType
-
-    this.source = env.blueprintComponents.rankings[key].source
-    this.ranking = env.blueprintComponents.rankings[key].factors
-    this.registry = env.bootedServices.registry.registry[key] // this should change to become the user-updated weights, rather than the already stored ones
+    this.rankings = env.blueprintComponents.rankings
+    this.registrys = env.bootedServices.registry.registry
     callback(null)
   }
 
   run (event, context) {
+    const schema = context.stateMachineMeta.namespace // e.g. test
+    const key = schema + '_' + this.propertyType // e.g. test_factory
+
     // registry maybe should come from event.registry
     generateView({
-      'propertyType': this.propertyType,
-      'schema': this.schema,
-      'source': this.source,
-      'ranking': this.ranking,
-      'registry': this.registry
-    })
-    // The function probably needs a callback
+      'propertyType': this.propertyType, // e.g. factory
+      'schema': schema,
+      'source': this.rankings[key].source,
+      'ranking': this.rankings[key].factors,
+      'registry': this.registrys[key]
+    }) // The function probably needs a callback
     context.sendTaskSuccess()
   }
 }
