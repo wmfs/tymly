3eaa79376647321c8085548b109001943db72993
Reece Brend <reece.brend@wmfs.net>
2017-10-13 07:55:26 +0100

    Adds state-resource for setting a registry key value

---
+++ packages/tymly/test/registry-state-tests.js
@@ -0,0 +1,59 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const path = require('path')
+const expect = require('chai').expect
+const tymly = require('../lib')
+const STATE_MACHINE_NAME = 'fbotTest_setRegistryKey_1_0'
+
+describe('It should test the state resource for setting reg keys', function () {
+  this.timeout(5000)
+
+  let statebox
+  let registry
+
+  it('Should run Tymly Service', function (done) {
+    tymly.boot(
+      {
+        blueprintPaths: [
+          path.resolve(__dirname, './fixtures/blueprints/animal-blueprint')
+        ]
+      },
+      function (err, tymlyServices) {
+        expect(err).to.equal(null)
+        statebox = tymlyServices.statebox
+        registry = tymlyServices.registry
+        done()
+      }
+    )
+  })
+
+  it('should get the value from registry using key', function (done) {
+    let key = 'fbotTest_mealThreshold'
+    let value = registry.get(key)
+    expect(value).to.eql(3)
+    done()
+  })
+
+  it('Should test the state resource execution', function (done) {
+    statebox.startExecution(
+      {},
+      STATE_MACHINE_NAME,
+      {
+        sendResponse: 'COMPLETE'
+      },
+      function (err, executionDescription) {
+        expect(err).to.equal(null)
+        done()
+      }
+    )
+  })
+
+  it('should get the value from registry using key after calling set', function (done) {
+    let key = 'fbotTest_mealThreshold'
+    let value = registry.get(key)
+    expect(value).to.eql(2)
+    done()
+  })
+})
