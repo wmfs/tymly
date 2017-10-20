5be79697688a20c7ff56f9a426d94edbc1ea9f8a
Tim Needham <tim.needham@wmfs.net>
2017-10-20 21:51:06 +0100

    Exposes auth0 for mocking.

--- plugins/tymly-express-plugin/lib/index.js
+++ plugins/tymly-express-plugin/lib/index.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  serverClass: require('./components/services/server')
+  serverClass: require('./components/services/server'),
+  authClass: require('./components/services/auth')
 }
