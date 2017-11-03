6011d7b12a92fa1cdaaa22f82c99bd543d3ff392
Meena Hoda <meena.hoda@wmfs.net>
2017-11-03 10:16:12 +0000

    Rankings - removed unnecessary if

--- plugins/tymly-rankings-plugin/lib/components/services/rankings/index.js
+++ plugins/tymly-rankings-plugin/lib/components/services/rankings/index.js
@@ -23,25 +23,24 @@ class RankingService {
       promises = rankingKeysWithValues.map(async (key) => {
         const value = rankings[key]
 
-        if (value.source && value.factors) {
-          await client.query(generateViewStatement({
-            category: _.snakeCase(value.name),
-            schema: _.snakeCase(value.namespace),
-            source: value.source,
-            ranking: value.factors,
-            registry: options.bootedServices.registry.registry[key]
-          }))
+        await client.query(generateViewStatement({
+          category: _.snakeCase(value.name),
+          schema: _.snakeCase(value.namespace),
+          source: value.source,
+          ranking: value.factors,
+          registry: options.bootedServices.registry.registry[key]
+        }))
 
-          // TODO: name should be inferred and not be 'test'
-          await generateStats({
-            client: client,
-            category: value.name,
-            schema: value.namespace,
-            pk: value.source.property,
-            name: 'test'
-          })
-        }
+        // TODO: name should be inferred and not be 'test'
+        await generateStats({
+          client: client,
+          category: value.name,
+          schema: value.namespace,
+          pk: value.source.property,
+          name: 'test'
+        })
       })
+
       Promise.all(promises)
         .then(() => callback(null))
         .catch((err) => callback(err))
