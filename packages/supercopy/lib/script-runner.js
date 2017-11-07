01e34165403320fed56e7463535f12af24898352
Jez Higgins <jez@jezuk.co.uk>
2017-11-07 16:38:12 +0000

    use client.run

--- packages/supercopy/lib/script-runner.js
+++ packages/supercopy/lib/script-runner.js
@@ -4,49 +4,5 @@ const async = require('async')
 const debug = require('debug')('supercopy')
 
 module.exports = function scriptRunner (statements, client, callback) {
-  let i = -1
-  async.eachSeries(
-    statements,
-    function (statementAndParam, cb) {
-      const statement = statementAndParam.sql
-      const action = statementAndParam.action
-
-      if (action) {
-        action(statement, null, client).
-          then(() => cb()).
-          catch(err => cb(err))
-      } else {
-        debug(`Running: ${statement}`)
-        client.query(
-          statement,
-          [],
-          cb
-        )
-      }
-    },
-    function (err) {
-      if (err) {
-        console.error('')
-        console.error('scriptRunner fail!')
-        console.error('------------------')
-        console.error()
-        console.error(JSON.stringify(statements[i], null, 2))
-        console.error(err)
-        console.error('')
-        client.query(
-          'ROLLBACK;',
-          function (rollbackErr) {
-            if (rollbackErr) {
-              console.error('FAILED TO ROLLBACK AS WELL! ' + rollbackErr)
-            } else {
-              console.log('ROLLBACK SUCCESSFUL.')
-            }
-            callback(err)
-          }
-        )
-      } else {
-        callback(null)
-      }
-    }
-  )
+  return client.run(statements, callback)
 }
