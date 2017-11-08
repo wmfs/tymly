9872683e4d26d6bce3d032fe62ce48ccb01de8b1
Jez Higgins <jez@jezuk.co.uk>
2017-11-08 12:54:38 +0000

    added runFile

--- packages/pg-client-helper/lib/index.js
+++ packages/pg-client-helper/lib/index.js
@@ -1,4 +1,5 @@
 const pg = require('pg')
+const fs = require('fs')
 const statementRunner = require('./pg-statement-runner')
 
 const NotSet = 'NotSet'
@@ -25,6 +26,16 @@ class PGClient {
       .then(result => callback(null, result))
       .catch(err => callback(err))
   } // run
+
+  runFile (fileName, callback = NotSet) {
+    const sql = fs.readFileSync(fileName, 'utf8')
+    const statements = sql.split(';').
+      map(s => s.replace(/\n/g, '')).
+      filter(s => s.length !== 0).
+      map(s => `${s};`).
+      map(s => { return { sql: s } })
+    return this.run(statements, callback)
+  } // runFromFile
 } // class PGClient
 
 module.exports = PGClient
