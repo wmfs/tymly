b671e81acec25933e99ccc5864eaf4c779c67cbf
Jez Higgins <jez@jezuk.co.uk>
2017-11-13 13:51:23 +0000

    Ensure we trigger callback when no rankings to apply

--- plugins/tymly-rankings-plugin/lib/components/services/rankings/index.js
+++ plugins/tymly-rankings-plugin/lib/components/services/rankings/index.js
@@ -10,44 +10,47 @@ class RankingService {
     const client = options.bootedServices.storage.client
     const rankings = options.blueprintComponents.rankings
 
-    if (_.isObject(rankings)) {
-      options.messages.info('Finding rankings')
-
-      let rankingKeysWithValues = Object.keys(rankings).filter(key => {
-        const value = rankings[key]
-        if (value.source && value.factors) {
-          return key
-        }
-      })
-
-      async.each(rankingKeysWithValues, (key, cb) => {
-        const value = rankings[key]
-        client.query(generateViewStatement({
-          category: _.snakeCase(value.name),
-          schema: _.snakeCase(value.namespace),
-          source: value.source,
-          ranking: value.factors,
-          registry: options.bootedServices.registry.registry[key]
-        }), (err) => {
-          if (err) cb(err)
-          // TODO: 'test' should be inferred
-          generateStats({
-            client: client,
-            category: value.name,
-            schema: value.namespace,
-            pk: value.source.property,
-            name: 'test'
-          }, (err) => {
-            if (err) cb(err)
-            cb()
-          })
-        })
-      }, (err) => {
-        if (err) callback(err)
-        console.log('Finished in ranking service')
-        callback(null)
-      })
+    if (!_.isObject(rankings)) {
+      options.messages.info('No rankings to find')
+      return callback(null)
     }
+
+    options.messages.info('Finding rankings')
+
+    const rankingKeysWithValues = Object.keys(rankings).filter(key => {
+      const value = rankings[key]
+      if (value.source && value.factors) {
+        return key
+      }
+    })
+
+    async.each(rankingKeysWithValues, (key, cb) => {
+      const value = rankings[key]
+      client.query(generateViewStatement({
+        category: _.snakeCase(value.name),
+        schema: _.snakeCase(value.namespace),
+        source: value.source,
+        ranking: value.factors,
+        registry: options.bootedServices.registry.registry[key]
+      }), (err) => {
+        if (err) cb(err)
+        // TODO: 'test' should be inferred
+        generateStats({
+          client: client,
+          category: value.name,
+          schema: value.namespace,
+          pk: value.source.property,
+          name: 'test'
+        }, (err) => {
+          if (err) cb(err)
+          cb()
+        })
+      })
+    }, (err) => {
+      if (err) callback(err)
+      console.log('Finished in ranking service')
+      callback(null)
+    })
   }
 }
 
