8ef36d1572c6ebe7196c1f12561496b7e537c51b
Liliana.Judge <liliana.judge@wmfs.net>
2017-11-15 15:27:35 +0000

    more favourites implementation

---
+++ plugins/tymly-users-plugin/test/fixtures/db-scripts/favourites/cleanup.sql
@@ -0,0 +1,2 @@
+DELETE FROM tymly.favouring-startable-state-machines WHERE user_id = 'test-user';
+DELETE FROM tymly.favouring-startable-state-machines WHERE user_id = 'test-user-1';
---
+++ plugins/tymly-users-plugin/test/fixtures/db-scripts/favourites/setup.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO tymly.favouring-startable-state-machines (user_id, favourite-startable-names)
+VALUES
+('test-user-1', '["settings", "watched-boards"]');
+
+INSERT INTO favouring-startable-state-machines (user_id, favourite-startable-names)
+VALUES
+('test-user', '["notifications", "settings"]');
+
