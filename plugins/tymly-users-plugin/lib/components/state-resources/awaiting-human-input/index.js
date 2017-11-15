36a140d144a5fc828d340c79e2762cac33076975
Reece Brend <reece.brend@wmfs.net>
2017-11-15 14:02:37 +0000

    Awaiting human input implementation and tests (including ability to default inputs)

--- plugins/tymly-users-plugin/lib/components/state-resources/awaiting-human-input/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/awaiting-human-input/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const jp = require('jsonpath')
+const _ = require('lodash')
+
 /*
 * TODO: What should this return in the context?
 * TODO: Handle failure if notification not found (look at tymly-mock-api)
@@ -8,9 +11,42 @@
 
 class AwaitingHumanInput {
   init (resourceConfig, env, callback) {
+    this.uiType = resourceConfig.uiType
+    this.uiName = resourceConfig.uiName
+    this.dataPath = resourceConfig.dataPath
+    this.defaults = resourceConfig.defaults
+    callback(null)
   }
 
-  run (event, context) {
+  run (event, context, done) {
+    let data
+
+    if (this.dataPath) {
+      data = jp.value(event, this.dataPath)
+    } else {
+      data = {}
+    }
+
+    if (this.defaults) {
+      data = _.defaults(data, this.defaults)
+    }
+
+    context.sendTaskHeartbeat(
+      {
+        requiredHumanInput: {
+          uiType: this.uiType,
+          uiName: this.uiName,
+          data: data
+        }
+      },
+      function (err, executionDescription) {
+        if (err) {
+          throw new Error(err)
+        } else {
+          done(executionDescription)
+        }
+      }
+    )
   }
 }
 
---
+++ plugins/tymly-users-plugin/test/awaiting-human-input-spec.js
@@ -0,0 +1,78 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const tymly = require('tymly')
+const path = require('path')
+const expect = require('chai').expect
+
+const HEARTBEAT_STATE_MACHINE = 'tymly_testHeartbeat_1_0'
+
+describe('awaitingUserInput state tests', function () {
+  this.timeout(5000)
+  let statebox
+
+  // const pgConnectionString = process.env.PG_CONNECTION_STRING
+  // const client = new PGClient(pgConnectionString)
+
+  it('should create some basic tymly services', function (done) {
+    tymly.boot(
+      {
+        pluginPaths: [
+          path.resolve(__dirname, './../lib'),
+          require.resolve('tymly-pg-plugin')
+        ]
+      },
+      function (err, tymlyServices) {
+        expect(err).to.eql(null)
+        statebox = tymlyServices.statebox
+        done()
+      }
+    )
+  })
+
+  it('should execute awaitingHumanInput state machine and expect defaults to come through', function (done) {
+    statebox.startExecution(
+      {},
+      HEARTBEAT_STATE_MACHINE,
+      {
+        sendResponse: 'AFTER_RESOURCE_CALLBACK.TYPE:awaitingHumanInput'
+      },
+      function (err, executionDescription) {
+        expect(err).to.eql(null)
+        expect(executionDescription.currentStateName).to.eql('TestHeartbeat')
+        expect(executionDescription.currentResource).to.eql('module:awaitingHumanInput')
+        expect(executionDescription.stateMachineName).to.eql(HEARTBEAT_STATE_MACHINE)
+        expect(executionDescription.status).to.eql('RUNNING')
+        expect(executionDescription.ctx.requiredHumanInput.data.empNo).to.eql(0)
+        expect(executionDescription.ctx.requiredHumanInput.data.status).to.eql('Probationary')
+        done()
+      }
+    )
+  })
+
+  it('should overwrite any default values if config passed in', function (done) {
+    statebox.startExecution(
+      {
+        someDefaultFormData: {
+          empNo: 14345,
+          status: 'Permanent'
+        }
+      },
+      HEARTBEAT_STATE_MACHINE,
+      {
+        sendResponse: 'AFTER_RESOURCE_CALLBACK.TYPE:awaitingHumanInput'
+      },
+      function (err, executionDescription) {
+        expect(err).to.eql(null)
+        expect(executionDescription.currentStateName).to.eql('TestHeartbeat')
+        expect(executionDescription.currentResource).to.eql('module:awaitingHumanInput')
+        expect(executionDescription.stateMachineName).to.eql(HEARTBEAT_STATE_MACHINE)
+        expect(executionDescription.status).to.eql('RUNNING')
+        expect(executionDescription.ctx.requiredHumanInput.data.empNo).to.eql(14345)
+        expect(executionDescription.ctx.requiredHumanInput.data.status).to.eql('Permanent')
+        done()
+      }
+    )
+  })
+})
