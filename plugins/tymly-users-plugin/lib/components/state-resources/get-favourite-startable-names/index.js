5c1b0371b78eddbe0a01dbc54dc54f07f3b20453
Liliana.Judge <liliana.judge@wmfs.net>
2017-11-15 15:36:37 +0000

    added code to the get-favourite-startable-names

--- plugins/tymly-users-plugin/lib/components/state-resources/get-favourite-startable-names/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/get-favourite-startable-names/index.js
@@ -1,15 +1,37 @@
 'use strict'
 
+const dottie = require('dottie')
+
 class GetFavouriteStartableNames {
   init (resourceConfig, env, callback) {
-    this.settings = env.bootedServices.storage.models['tymly_settings']
+    this.favouringStartableStateMachines = env.bootedServices.storage.models['tymly_favouringStartableStateMachines']
     callback(null)
   }
 
   run (event, context) {
     const userId = context.userId
-    console.log('!!!!' + userId)
-    context.sendTaskSuccess()
+    let executionDescription = {}
+    this.favouringStartableStateMachines.find(
+      {
+        where: {
+          userId: {equals: userId}
+        }
+      },
+      (err, results) => {
+        if (err) {
+          console.log('ERROR')
+          context.sendTaskFailure(
+            {
+              error: 'getFavouriteStartableNamesFail',
+              cause: err
+            }
+          )
+        } else {
+          dottie.set(executionDescription, 'favouriteStartableNames', results)
+          context.sendTaskSuccess({results})
+        }
+      }
+    )
   }
 }
 
