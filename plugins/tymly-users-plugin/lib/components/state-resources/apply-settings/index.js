ac3ed5d6a60afcbf2b0ec40c3c1625582d9fa86e
Meena Hoda <meena.hoda@wmfs.net>
2017-11-16 09:56:19 +0000

    some extra expects in user plugin tests

--- plugins/tymly-users-plugin/lib/components/state-resources/apply-settings/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/apply-settings/index.js
@@ -1,11 +1,5 @@
 'use strict'
 
-/*
-* TODO: What should this return in the context?
-* TODO: Handle failure if notification not found (look at tymly-mock-api)
-* TODO: pg-model and tymly/storage/memory-model need to handle dates
-* */
-
 class ApplySettings {
   init (resourceConfig, env, callback) {
     this.settings = env.bootedServices.storage.models['tymly_settings']
