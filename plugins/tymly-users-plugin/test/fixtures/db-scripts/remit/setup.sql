0101ad5bdc93af4dd4174f27738c7e32fa5d98e4
Meena Hoda <meena.hoda@wmfs.net>
2017-11-20 13:07:45 +0000

    safer set up and clean up of user remit test scripts

--- plugins/tymly-users-plugin/test/fixtures/db-scripts/remit/setup.sql
+++ plugins/tymly-users-plugin/test/fixtures/db-scripts/remit/setup.sql
@@ -1,14 +1,14 @@
-INSERT INTO tymly.categories (label, description)
-VALUES ('Gazetteer', 'Things to do with individual buildings and other property');
-INSERT INTO tymly.categories (label, description)
-VALUES ('Fire', 'Fire-related incidents');
-INSERT INTO tymly.categories (label, description)
-VALUES ('Water', 'All things water and hydrants');
+INSERT INTO tymly.categories (label, description, id)
+VALUES ('Gazetteer', 'Things to do with individual buildings and other property', 'a69c0aa1-cde5-11e7-abc4-cec278b6b50a');
+INSERT INTO tymly.categories (label, description, id)
+VALUES ('Fire', 'Fire-related incidents', 'a69c0ab2-cde5-11e7-abc4-cec278b6b50a');
+INSERT INTO tymly.categories (label, description, id)
+VALUES ('Water', 'All things water and hydrants', 'a69c0ac3-cde5-11e7-abc4-cec278b6b50a');
 
-INSERT INTO tymly.teams (title, description)
-VALUES ('Fire Safety (North)', 'Fire Safety Team for the northern region');
-INSERT INTO tymly.teams (title, description)
-VALUES ('Birmingham (Red watch)', 'Birmingham Fire Station (Red Watch)');
+INSERT INTO tymly.teams (title, description, id)
+VALUES ('Fire Safety (North)', 'Fire Safety Team for the northern region', 'a69c0ad4-cde5-11e7-abc4-cec278b6b50a');
+INSERT INTO tymly.teams (title, description, id)
+VALUES ('Birmingham (Red watch)', 'Birmingham Fire Station (Red Watch)', 'a69c0ae5-cde5-11e7-abc4-cec278b6b50a');
 
 INSERT INTO tymly.todos (state_machine_title, state_machine_category, todo_title, id)
 VALUES ('Process expense claim', 'expenses', 'Homer Simpson', 'a69c0ac9-cde5-11e7-abc4-cec278b6b50a');
