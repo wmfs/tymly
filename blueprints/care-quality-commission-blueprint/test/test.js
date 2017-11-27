451008e862db189a8393790a5e1ee668faf2c63f
Reece Brend <reece.brend@wmfs.net>
2017-11-27 11:19:01 +0000

    cqc blueprint removal of test data after test execution

--- blueprints/care-quality-commission-blueprint/test/test.js
+++ blueprints/care-quality-commission-blueprint/test/test.js
@@ -55,72 +55,100 @@ describe('CQC tests', function () {
 
   it('should be the correct data in the database', function (done) {
     client.query(
-        'select location_id, location_name, latest_rating ' +
-        'from cqc.cqc ' +
-        'order by location_id',
-        function (err, result) {
-          expect(err).to.eql(null)
-          if (err) {
-            console.error(err)
-          } else {
-            expect(result.rows).to.eql(
-              [
-                {
-                  location_id: '1-1004508435',
-                  location_name: 'Meadow Rose Nursing Home',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1010722651',
-                  location_name: 'MBI Home Care Ltd',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1011039037',
-                  location_name: 'Balm Care Services Limited',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1012933568',
-                  location_name: 'Care for You (UK) Limited',
-                  latest_rating: 'Requires improvement'
-                },
-                {
-                  location_id: '1-1017570228',
-                  location_name: 'Bluebell Centre',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1028678661',
-                  location_name: 'Cygnet House',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1028986728',
-                  location_name: 'Ambleside',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1029431911',
-                  location_name: 'Ashley House',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1040494951',
-                  location_name: 'Ross Court Care Home',
-                  latest_rating: 'Good'
-                },
-                {
-                  location_id: '1-1043708006',
-                  location_name: 'Beverley Mews',
-                  latest_rating: 'Good'
-                }
-              ]
-            )
-            done()
-          }
+      'select location_id, location_name, latest_rating ' +
+      'from cqc.cqc ' +
+      'order by location_id',
+      function (err, result) {
+        expect(err).to.eql(null)
+        if (err) {
+          console.error(err)
+        } else {
+          expect(result.rows).to.eql(
+            [
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567890',
+                'location_name': 'Meadow Rose Nursing Home'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '12345678909',
+                'location_name': 'Beverley Mews'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567891',
+                'location_name': 'MBI Home Care Ltd'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567892',
+                'location_name': 'Balm Care Services Limited'
+              },
+              {
+                'latest_rating': 'Requires improvement',
+                'location_id': '1234567893',
+                'location_name': 'Care for You (UK) Limited'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567894',
+                'location_name': 'Bluebell Centre'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567895',
+                'location_name': 'Cygnet House'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567896',
+                'location_name': 'Ambleside'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567897',
+                'location_name': 'Ashley House'
+              },
+              {
+                'latest_rating': 'Good',
+                'location_id': '1234567898',
+                'location_name': 'Ross Court Care Home'
+              }
+            ]
+          )
+          done()
         }
-      )
-  }
-  )
+      }
+    )
+  })
+
+  it('Should be clean up the database', function (done) {
+    client.query(
+      `DELETE FROM cqc.cqc WHERE location_id::text LIKE '123456789%';`,
+      function (err, result) {
+        expect(err).to.equal(null)
+        if (err) {
+          done(err)
+        } else {
+          done()
+        }
+      }
+    )
+  })
+
+  it('Should find a now empty database', function (done) {
+    client.query(
+      'select * from cqc.cqc;',
+      function (err, result) {
+        expect(err).to.equal(null)
+        if (err) {
+          done(err)
+        } else {
+          expect(result.rows).to.eql([])
+          done()
+        }
+      }
+    )
+  })
 })
