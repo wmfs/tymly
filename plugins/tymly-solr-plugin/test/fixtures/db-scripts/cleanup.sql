590fa882d70dbfb550369f479979ef6a97f86d17
Meena Hoda <meena.hoda@wmfs.net>
2017-11-29 11:30:49 +0000

    drop the table at end of test

--- plugins/tymly-solr-plugin/test/fixtures/db-scripts/cleanup.sql
+++ plugins/tymly-solr-plugin/test/fixtures/db-scripts/cleanup.sql
@@ -1,5 +1,5 @@
 DROP VIEW IF EXISTS tymly.solr_data;
-
+DROP TABLE IF EXISTS tymly.search_history;
 DROP TABLE IF EXISTS tymly_test.students;
 DROP TABLE IF EXISTS tymly_test.staff;
 DROP SCHEMA IF EXISTS tymly_test;
