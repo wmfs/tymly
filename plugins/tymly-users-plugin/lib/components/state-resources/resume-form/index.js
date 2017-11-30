3548edcf6943f5607c99b6194ef1e0314f39b9e0
Meena Hoda <meena.hoda@wmfs.net>
2017-11-30 09:52:33 +0000

    resume form implementation

--- plugins/tymly-users-plugin/lib/components/state-resources/resume-form/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/resume-form/index.js
@@ -2,11 +2,16 @@
 
 class ResumeForm {
   init (resourceConfig, env, callback) {
+    this.models = env.bootedServices.storage.models
     callback(null)
   }
 
   run (event, context) {
-    context.sendTaskSuccess()
+    const model = this.models[`tymly_${event.model}`]
+    model.findById(event.formId, (err, doc) => {
+      if (err) context.sendTaskFailure(err)
+      context.sendTaskSuccess({form: doc})
+    })
   }
 }
 
