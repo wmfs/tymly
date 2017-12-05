bcfab7fb1668b32f814e2750f965ae197e184f5d
Martina Henry <martina.henry@wmfs.net>
2017-12-05 14:11:01 +0000

    Basic test for the fire safety short audit

---
+++ blueprints/demo-blueprint/test/fire-safety-tests.js
@@ -0,0 +1,73 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const tymly = require('tymly')
+const path = require('path')
+const expect = require('chai').expect
+const sqlScriptRunner = require('./fixtures/sql-script-runner.js')
+
+describe('Demo state machine tests', function () {
+  this.timeout(process.env.TIMEOUT || 5000)
+  const FILL_FIRE_SAFETY_SHORT_AUDIT_STATE_MACHINE = 'tymly_fillFireSafetyShortAudit_1_0'
+  let statebox, client, fillFireSafetyShortAuditExecutionName, fireSafetyShortAudit
+
+  const formData = {
+    ignitionSources: 'Good',
+    ignitionSourcesComment: 'Testing',
+    adequateMeasures: 'Good',
+    adequateMeasuresComment: 'Testing',
+    personsEvacuate: 'Good',
+    personsEvacuateComment: 'Testing',
+    escapeRoutes: 'Good',
+    escapeRoutesComment: 'Testing',
+    adequateEquipment: 'Good',
+    adequateEquipmentComment: 'Testing',
+    adequateArrangements: 'Good',
+    adequateArrangementsComment: 'Testing',
+    adequateInstructions: 'Good',
+    adequateInstructionsComment: 'Testing',
+    adequateManagement: 'Good',
+    adequateManagementComment: 'Testing',
+    sufficientPrecautions: 'Yes'
+  }
+
+  it('should startup tymly', function (done) {
+    tymly.boot(
+      {
+        pluginPaths: [
+          require.resolve('tymly-pg-plugin'),
+          require.resolve('tymly-solr-plugin'),
+          require.resolve('tymly-users-plugin')
+        ],
+        blueprintPaths: [
+          path.resolve(__dirname, './../')
+        ],
+        config: {}
+      },
+      function (err, tymlyServices) {
+        statebox = tymlyServices.statebox
+        client = tymlyServices.storage.client
+        fireSafetyShortAudit = tymlyServices.storage.models['tymly_fireSafetyShortAudit']
+        done(err)
+      }
+    )
+  })
+
+  it('should start execution to claim expense, stops at AwaitingHumanInput', function (done) {
+    statebox.startExecution(
+      {},
+      FILL_FIRE_SAFETY_SHORT_AUDIT_STATE_MACHINE,
+      {
+        sendResponse: 'AFTER_RESOURCE_CALLBACK.TYPE:awaitingHumanInput'
+      },
+      (err, executionDescription) => {
+        expect(err).to.eql(null)
+        expect(executionDescription.currentStateName).to.eql('AwaitingHumanInput')
+        expect(executionDescription.status).to.eql('RUNNING')
+        fillFireSafetyShortAuditExecutionName = executionDescription.executionName
+        done(err)
+      }
+    )
+  })
+})
