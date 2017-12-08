512539f103f0468078dd26b2d7f8e4059170c6e7
Meena.Hoda <meena.hoda@wmfs.net>
2017-12-08 15:42:05 +0000

    board model missing category property

--- plugins/tymly-users-plugin/lib/components/state-resources/watch-board/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/watch-board/index.js
@@ -7,6 +7,7 @@ class WatchBoard {
   }
 
   run (event, context) {
+    console.log('event >>>', event)
     let feedName = [event.stateMachineName]
     if (event.key) Object.keys(event.key).sort().map(k => feedName.push(event.key[k]))
     feedName = feedName.join('|')
