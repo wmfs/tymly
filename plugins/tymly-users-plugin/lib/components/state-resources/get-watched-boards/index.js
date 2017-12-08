ac134d1dd2c43002c76238e9867f46ec3f2d1b9f
Meena.Hoda <meena.hoda@wmfs.net>
2017-12-08 16:17:24 +0000

    removed todo comment

--- plugins/tymly-users-plugin/lib/components/state-resources/get-watched-boards/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/get-watched-boards/index.js
@@ -1,9 +1,5 @@
 'use strict'
 
-/*
-* TODO: Boards should be categorized - this will affect implementation here
-* */
-
 class GetWatchedBoards {
   init (resourceConfig, env, callback) {
     this.watchedBoards = env.bootedServices.storage.models['tymly_watchedBoards']
