8dfc3bc56f33d2fe9458e9a43d22317c0c7dc840
Meena.Hoda <meena.hoda@wmfs.net>
2017-12-13 07:45:12 +0000

    camel case to get models

--- plugins/tymly-users-plugin/lib/components/state-resources/get-board/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/get-board/index.js
@@ -1,16 +1,14 @@
 'use strict'
 
-const _ = require('lodash')
-
 class GetBoard {
   init (resourceConfig, env, callback) {
     this.models = env.bootedServices.storage.models
-    this.modelName = _.snakeCase(resourceConfig.model)
+    this.modelName = resourceConfig.model
     callback(null)
   }
 
   run (event, context) {
-    const model = this.models[`${_.snakeCase(context.stateMachineMeta.namespace)}_${this.modelName}`]
+    const model = this.models[`${context.stateMachineMeta.namespace}_${this.modelName}`]
     const where = {}
 
     Object.keys(event).map(k => {
