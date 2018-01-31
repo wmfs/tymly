38e63a24e5aeaa7881e564f4c58191bb36aeaeef
Jas.Bahra <jas.bahra@wmfs.net>
2018-01-31 16:54:45 +0000

    In cases where the response has a statusCode of 500, fixed bug which didn't actually send the response

--- plugins/tymly-express-plugin/lib/components/services/statebox-api/routes/actions/Send-task-success.js
+++ plugins/tymly-express-plugin/lib/components/services/statebox-api/routes/actions/Send-task-success.js
@@ -7,7 +7,8 @@ module.exports = function sendTaskSuccess (statebox, req, res, env) {
     {},
     function (err) {
       if (err) {
-        res.status(500)
+        console.error(err)
+        res.status(500).send()
       } else {
         res.status(200).send()
       }
