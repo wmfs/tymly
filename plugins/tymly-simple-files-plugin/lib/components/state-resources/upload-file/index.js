52abd8cc9bd6fde3851f9261888fa83c7f88c5b3
Martina Henry <martina.henry@wmfs.net>
2018-02-01 10:14:00 +0000

    file saved to file system.. hardcoded

--- plugins/tymly-simple-files-plugin/lib/components/state-resources/upload-file/index.js
+++ plugins/tymly-simple-files-plugin/lib/components/state-resources/upload-file/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var fs = require('file-system')
+
 class UploadFile {
   init (resourceConfig, env, callback) {
     callback(null)
@@ -8,7 +10,12 @@ class UploadFile {
   run (event, context) {
     console.log('In Upload File')
     console.log(event)
-    console.log(context)
+    console.log(event.base64)
+
+    fs.writeFile(`C:\\` + event.fileName, event.base64, function (err) {
+      console.log(err)
+    })
+
     context.sendTaskSuccess(event.fileName)
   }
 }
