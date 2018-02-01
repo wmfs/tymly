c62e246b6b66d07b5950d8b467ed2b1d1ac14f5b
Martina Henry <martina.henry@wmfs.net>
2018-02-01 14:01:31 +0000

    files saved with uuid and not the actual names

--- plugins/tymly-simple-files-plugin/lib/components/state-resources/upload-file/index.js
+++ plugins/tymly-simple-files-plugin/lib/components/state-resources/upload-file/index.js
@@ -5,7 +5,8 @@ const base64 = require('base-64')
 
 class UploadFile {
   init (resourceConfig, env, callback) {
-    console.log(`----->`, env.bootedServices.storage.models)
+    console.log(`----->`, env)
+    console.log(`----->`, resourceConfig)
     this.files = env.bootedServices.storage.models['tymly_files']
     callback(null)
   }
@@ -20,10 +21,6 @@ class UploadFile {
 
     const decodedData = base64.decode(actualbase64String)
 
-    fs.writeFile(`C:\\` + event.fileName, decodedData, function (err) {
-      console.log(err)
-    })
-
     this.files.upsert(
       {
         fileName: event.fileName
@@ -31,6 +28,10 @@ class UploadFile {
       {}
     )
       .then((doc) => {
+        fs.writeFile(`C:\\` + doc.idProperties.id, decodedData, function (err) {
+          console.log(err)
+        })
+
         context.sendTaskSuccess({
           fileId: doc.idProperties.id,
           fileName: event.fileName
