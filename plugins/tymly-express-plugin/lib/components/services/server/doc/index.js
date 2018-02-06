e4cde9d48a8b79369c4a05ec9140e5ba60e5219e
Martina Henry <martina.henry@wmfs.net>
2018-02-06 15:37:35 +0000

    Typo removed

--- plugins/tymly-express-plugin/lib/components/services/server/doc/index.js
+++ plugins/tymly-express-plugin/lib/components/services/server/doc/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  description: 'Provides a CORS-enabled Express.js server. Includes serving of static assets as supplied via Bluepints',
+  description: 'Provides a CORS-enabled Express.js server. Includes serving of static assets as supplied via Blueprints',
   example: require('./example.json'),
   blueprintDirs: {
     images: 'A place to put images that can be served-up in Forms and similar'
