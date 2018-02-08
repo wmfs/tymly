8d82419fabbc51c3b270eb162c340ace900bd92e
Meena.Hoda <meena.hoda@wmfs.net>
2018-02-08 08:58:58 +0000

    and shutdown tymly

--- plugins/tymly-pg-plugin/test/audit-spec.js
+++ plugins/tymly-pg-plugin/test/audit-spec.js
@@ -9,6 +9,8 @@ const path = require('path')
 describe('PostgreSQL storage tests', function () {
   this.timeout(process.env.TIMEOUT || 5000)
 
+  let tymlyService
+
   it('should create some tymly services', (done) => {
     tymly.boot(
       {
@@ -18,8 +20,13 @@ describe('PostgreSQL storage tests', function () {
       },
       (err, tymlyServices) => {
         expect(err).to.eql(null)
+        tymlyService = tymlyServices.tymly
         done(err)
       }
     )
+  })
+
+  it('should shutdown Tymly', async () => {
+    await tymlyService.shutdown()
   })
 })
