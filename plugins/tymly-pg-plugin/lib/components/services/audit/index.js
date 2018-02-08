153031ef43084110b4322048e2887b68bff34b5d
Meena.Hoda <meena.hoda@wmfs.net>
2018-02-08 09:40:11 +0000

    audit service to run pg scripts

--- plugins/tymly-pg-plugin/lib/components/services/audit/index.js
+++ plugins/tymly-pg-plugin/lib/components/services/audit/index.js
@@ -1,10 +1,33 @@
 'use strict'
 
 const schema = require('./schema.json')
+const HlPgClient = require('hl-pg-client')
 
 class AuditService {
   boot (options, callback) {
-    callback(null)
+    const connectionString = process.env.PG_CONNECTION_STRING || ''
+    this.pgScripts = options.blueprintComponents.pgScripts || {}
+
+    if (connectionString) {
+      this.client = new HlPgClient(connectionString)
+
+      const promises = Object.keys(this.pgScripts).map(script => {
+        return this.client.runFile(this.pgScripts[script].filePath)
+      })
+
+      Promise.all(promises)
+        .then(() => {
+          console.log('done')
+          callback(null)
+        })
+        .catch(err => {
+          console.log('ERR:', err)
+        })
+    }
+  }
+
+  async shutdown () {
+    await this.client.end()
   }
 }
 
