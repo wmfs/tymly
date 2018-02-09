c03f1a4db6a9ef9da1d5f36483e2281b0a6f01cf
Jez Higgins <jez@jezuk.co.uk>
2018-02-09 14:12:32 +0000

    Rework audit service startup so that it installs all its triggers before signalling completion

--- plugins/tymly-pg-plugin/lib/components/services/audit/index.js
+++ plugins/tymly-pg-plugin/lib/components/services/audit/index.js
@@ -9,63 +9,63 @@ const pgInfo = require('pg-info')
 
 class AuditService {
   boot (options, callback) {
-    const connectionString = process.env.PG_CONNECTION_STRING || ''
     this.pgScripts = options.blueprintComponents.pgScripts || {}
     this.models = options.blueprintComponents.models || {}
     this.client = options.bootedServices.storage.client
     this.schemaNames = options.bootedServices.storage.schemaNames
 
-    if (connectionString) {
-      this.auditFunctions = []
+    this.auditFunctions = []
 
-      const promises = Object.keys(this.pgScripts).map(script => {
-        debug(`Found script: ${script}`)
-        const filename = path.parse(this.pgScripts[script].filename).name
-        if (filename.split('-')[0] === 'audit') {
-          debug(`Found audit function: ${filename.substring(filename.indexOf('-') + 1)}`)
-          this.auditFunctions.push(filename.substring(filename.indexOf('-') + 1))
-          return this.client.runFile(this.pgScripts[script].filePath)
-        }
-      })
+    const functionInstallers = Object.keys(this.pgScripts).map(script => {
+      debug(`Found script: ${script}`)
+      const filename = path.parse(this.pgScripts[script].filename).name
+      if (filename.split('-')[0] === 'audit') {
+        debug(`Found audit function: ${filename.substring(filename.indexOf('-') + 1)}`)
+        this.auditFunctions.push(filename.substring(filename.indexOf('-') + 1))
+        return this.client.runFile(this.pgScripts[script].filePath)
+      }
+    })
 
-      Promise.all(promises)
-        .then(async () => {
-          await this.updateTriggers()
-          callback(null)
-        })
-        .catch(err => callback(err))
-    }
-  }
+    Promise.all(functionInstallers)
+      .then(() => this.updateTriggers())
+      .then(() => callback(null))
+      .catch(err => callback(err))
+  } // boot
 
   updateTriggers () {
-    this.auditFunctions.map(func => {
-      Object.keys(this.models).map(async model => {
-        const audit = this.models[model].audit !== false
-
-        const namespace = _.snakeCase(this.models[model].namespace)
-        const name = _.snakeCase(this.models[model].name)
-        const triggerName = `${namespace}_${name}_auditor`
-
-        const currentDbStructure = await pgInfo({
-          client: this.client,
-          schemas: this.schemaNames
-        })
-
-        const currentTriggers = currentDbStructure.schemas[namespace].tables[name].triggers
-        const trigger = Object.keys(currentTriggers).includes(triggerName)
-        const action = (!trigger && audit) ? 'ADD' : ((trigger && !audit) ? 'REMOVE' : 'NONE')
-        debug(`Model: ${model}, Wants to audit: ${audit}, Already has trigger: ${trigger}, Action: ${action}`)
-
-        const triggerSQL = generateTriggerStatement({
-          model: this.models[model],
-          function: func,
-          action: action
-        })
-
-        await this.client.query(triggerSQL)
-      })
+    const allInstallers = this.auditFunctions.map(func => {
+      const installers = Object.keys(this.models).map(model => this.installTrigger(func, model))
+      return Promise.all(installers)
     })
-  }
+    return Promise.all(allInstallers)
+  } // updateTriggers
+
+  async installTrigger (func, model) {
+    const audit = this.models[model].audit !== false
+
+    const namespace = _.snakeCase(this.models[model].namespace)
+    const name = _.snakeCase(this.models[model].name)
+    const triggerName = `${namespace}_${name}_auditor`
+
+    const currentDbStructure = await pgInfo({
+      client: this.client,
+      schemas: this.schemaNames
+    })
+
+    const triggers = currentDbStructure.schemas[namespace].tables[name].triggers
+    const trigger = Object.keys(triggers).includes(triggerName)
+    const action = (!trigger && audit) ? 'ADD' : ((trigger && !audit) ? 'REMOVE' : '')
+    debug(`Model: ${model}, Wants to audit: ${audit}, Already has trigger: ${trigger}, Action: ${action}`)
+
+    const triggerSQL = generateTriggerStatement({
+      model: this.models[model],
+      function: func,
+      action: action
+    })
+
+    debug(`Installing trigger for ${model}`)
+    return this.client.query(triggerSQL)
+  } // installTrigger
 }
 
 module.exports = {
