e04cc9d2f71d1caf26aeb9e488f763ff32996696
Meena.Hoda <meena.hoda@wmfs.net>
2018-02-13 14:11:06 +0000

    and the test scripts

--- plugins/tymly-solr-plugin/test/fixtures/db-scripts/cleanup.sql
+++ plugins/tymly-solr-plugin/test/fixtures/db-scripts/cleanup.sql
@@ -2,4 +2,5 @@ DROP VIEW IF EXISTS tymly.solr_data;
 DROP TABLE IF EXISTS tymly.search_history;
 DROP TABLE IF EXISTS tymly_test.students;
 DROP TABLE IF EXISTS tymly_test.staff;
+DROP TABLE IF EXISTS tymly_test.incident;
 DROP SCHEMA IF EXISTS tymly_test;
---
+++ plugins/tymly-solr-plugin/test/fixtures/db-scripts/incident-setup.sql
@@ -0,0 +1,4 @@
+INSERT INTO tymly_test.incident (inc_no, description) VALUES (1, 'A bad incident');
+INSERT INTO tymly_test.incident (inc_no, description) VALUES (2, 'A very bad incident');
+INSERT INTO tymly_test.incident (inc_no, description) VALUES (3, 'A not so bad incident');
+
