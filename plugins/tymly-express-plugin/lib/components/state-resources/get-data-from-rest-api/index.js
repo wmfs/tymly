0d531124f8d1a958f5be23209aed2c4bc7c1089b
Meena.Hoda <meena.hoda@wmfs.net>
2018-02-15 08:20:31 +0000

    rebuild url outside of init

--- plugins/tymly-express-plugin/lib/components/state-resources/get-data-from-rest-api/index.js
+++ plugins/tymly-express-plugin/lib/components/state-resources/get-data-from-rest-api/index.js
@@ -4,15 +4,17 @@ const requestPromise = require('request-promise-native')
 
 class GetDataFromRestApi {
   init (resourceConfig, env, callback) {
-    const registry = env.bootedServices.registry
-    this.templateUrl = registry.get(resourceConfig.namespace + '_' + resourceConfig.templateUrlRegistryKey)
-    if (resourceConfig.authTokenRegistryKey) this.authToken = registry.get(resourceConfig.namespace + '_' + resourceConfig.authTokenRegistryKey)
+    this.registry = env.bootedServices.registry
+    this.namespace = resourceConfig.namespace
+    this.templateUrlRegistryKey = resourceConfig.templateUrlRegistryKey
+    if (resourceConfig.authTokenRegistryKey) this.authToken = this.registry.get(resourceConfig.namespace + '_' + resourceConfig.authTokenRegistryKey)
     if (resourceConfig.resultPath) this.resultPath = resourceConfig.resultPath
     if (resourceConfig.paramPath) this.paramPath = resourceConfig.paramPath
     callback(null)
   }
 
   run (event, context) {
+    this.templateUrl = this.registry.get(this.namespace + '_' + this.templateUrlRegistryKey)
     if (this.paramPath) {
       Object.keys(event[this.paramPath]).map(key => {
         this.templateUrl = this.templateUrl.replace(`{{${key}}}`, event[this.paramPath][key])
