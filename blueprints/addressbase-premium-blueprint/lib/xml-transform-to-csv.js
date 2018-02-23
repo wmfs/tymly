deadb847cb75952910be795b7b9515ea2410b259
Jez Higgins <jez@jezuk.co.uk>
2018-02-23 11:19:46 +0000

    processSubTree is synchronous so make a generator

--- blueprints/addressbase-premium-blueprint/lib/xml-transform-to-csv.js
+++ blueprints/addressbase-premium-blueprint/lib/xml-transform-to-csv.js
@@ -5,25 +5,21 @@ const simplifyJson = require('./simplify-json')
 const flattenJson = require('./flatten-json-to-csv')
 const jp = require('jsonpath')
 
-function processSubtree (
+function* processSubtree (
   subTree,
   pivotPath,
   selectPaths
 ) {
   const cleanTree = simplifyJson(subTree)
 
-  return new EachPromise((each, resolve, reject) => {
-    const pivots = jp.query(cleanTree, pivotPath)[0]
-    for (let i = 0; i !== pivots.length; ++i) {
-      const contextPath = `${pivotPath}[${i}]`
+  const pivots = jp.query(cleanTree, pivotPath)[0]
+  for (let i = 0; i !== pivots.length; ++i) {
+    const contextPath = `${pivotPath}[${i}]`
 
-      const csv = flattenJson(cleanTree, contextPath, selectPaths)
+    const csv = flattenJson(cleanTree, contextPath, selectPaths)
 
-      each(csv)
-    }
-
-    resolve()
-  })
+    yield(csv)
+  }
 } // processSubtree
 
 function xmlTransformToCsv (
@@ -34,9 +30,10 @@ function xmlTransformToCsv (
 ) {
   return new EachPromise((each, resolve, reject) => {
     xmlSubtreeProcessor(inputStream, elementName)
-      .each(async subTree => {
-        await processSubtree(subTree, pivotPath, selectPaths)
-          .each(line => each(line))
+      .each(subTree => {
+          for (const line of processSubtree(subTree, pivotPath, selectPaths)) {
+              each(line)
+          }
       })
       .then(() => resolve())
       .catch(err => reject(err))
