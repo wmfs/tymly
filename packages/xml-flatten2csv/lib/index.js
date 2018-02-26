a2e513e17d8408d6ad771c84fd7dc8614fa70a91
Jez Higgins <jez@jezuk.co.uk>
2018-02-26 13:51:35 +0000

    filling out xmlFlatten2Xml

--- packages/xml-flatten2csv/lib/index.js
+++ packages/xml-flatten2csv/lib/index.js
@@ -1,17 +1,58 @@
+
+const path = require('path')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const promisify = require('util').promisify
+
+const pexists = promisify(fs.exists)
+const pmkdirp = promisify(mkdirp)
+
 const xmlTransformToCsv = require('./xml-transform-to-csv')
 
-function selectPaths (headerMap) { }
+function error (message, code, path) {
+  const err = new Error(message)
+  if (code) err.code = code
+  if (path) err.path = path
+  return err
+} // error
 
-function openInputStream (xmlPath) { }
-function createOutputStream (csvPath) { }
 
-function writeLine (outputFields, fields, headerMap) { }
+function selectPaths (headerMap) {
+  if (!headerMap || headerMap.length === 0) {
+    throw error('headerMap must not be empty')
+  } // selectPaths
+
+  return headerMap.map(fields => fields[0])
+} // headerMap
+
+async function openInputStream (xmlPath) {
+  if (!xmlPath) throw error('xmlPath must be set')
+
+  if (!await pexists(xmlPath)) {
+    throw error(`File ${xmlPath} not found`, 'ENOENT', xmlPath)
+  }
+
+  const source = fs.createReadStream(xmlPath)
+  return source
+} // openInputStream
+
+async function createOutputStream (csvPath) {
+  if (!csvPath) throw error('csvPath must be set')
+
+  const outputDir = path.dirname(csvPath)
+  await pmkdirp(outputDir)
+  return fs.createWriteStream(csvPath)
+} // createOutputStream
+
+function writeLine (outputStream, fields, headerMap) {
+  console.log(fields)
+} // writeLine
 
 async function xmlFlatten2Csv(options) {
   const selectors = selectPaths(options.headerMap)
 
-  const inputStream = openInputStream(options.xmlPath)
-  const outputStream = createOutputStream(options.csvPath)
+  const inputStream = await openInputStream(options.xmlPath)
+  const outputStream = await createOutputStream(options.csvPath)
 
   try {
     xmlTransformToCsv(
--- packages/xml-flatten2csv/test/xml-flatten2csv-tests.js
+++ packages/xml-flatten2csv/test/xml-flatten2csv-tests.js
@@ -3,9 +3,10 @@
 const path = require('path')
 const fs = require('fs')
 const expect = require('chai').expect
+const assert = require('chai').assert
 const xmlFlatten2csv = require('../lib')
 
-describe('xmlFlatten2CSV', function () {
+describe('xmlFlatten2csv', () => {
   it('convert simple xml to csv', async () => {
     const sourceFile = path.resolve(__dirname, 'fixtures', 'simpsons.xml')
     const outputFile = path.resolve(__dirname, 'output', 'simpsons.csv')
@@ -17,12 +18,11 @@ describe('xmlFlatten2CSV', function () {
       xmlPath: sourceFile,
       csvPath: outputFile,
       rootXMLElement: 'Episode',
-      pivotPath: '$.Person',
+      pivotPath: '$.People.Person',
       headerMap: [
         ['$.Title', 'title', 'string'],
         ['@.Name', 'name', 'string'],
         ['@.Age', 'age', 'integer'],
-        ['@.Gender', 'gender', 'string'],
         ['@.Siblings.Brother', 'brother', 'string'],
         ['@.Siblings.Sister', 'sister', 'string']
       ]
@@ -33,4 +33,72 @@ describe('xmlFlatten2CSV', function () {
 
     expect(output).to.eql(expected)
   })
+
+  describe('error cases', () => {
+    const sourceFile = path.resolve(__dirname, 'fixtures', 'simpsons.xml')
+    const outputFile = path.resolve(__dirname, 'output', 'fail.csv')
+
+    it('bad input file', async () => {
+      try {
+        await xmlFlatten2csv({
+          xmlPath: 'i-do-not-exist',
+          csvPath: outputFile,
+          rootXMLElement: 'Episode',
+          pivotPath: '$.People.Person',
+          headerMap: [
+            ['$.Title', 'title', 'string'],
+            ['@.Name', 'name', 'string'],
+            ['@.Age', 'age', 'integer'],
+            ['@.Siblings.Brother', 'brother', 'string'],
+            ['@.Siblings.Sister', 'sister', 'string']
+          ]
+        })
+      } catch (err) {
+        expect(err.code).to.equal('ENOENT')
+        return
+      }
+
+      assert.fail('Did not throw')
+    })
+
+    it('bad output file', async () => {
+      try {
+        await xmlFlatten2csv({
+          xmlPath: sourceFile,
+          csvPath: '/root/can#t"be/created',
+          rootXMLElement: 'Episode',
+          pivotPath: '$.Person',
+          headerMap: [
+            ['$.Title', 'title', 'string'],
+            ['@.Name', 'name', 'string'],
+            ['@.Age', 'age', 'integer'],
+            ['@.Siblings.Brother', 'brother', 'string'],
+            ['@.Siblings.Sister', 'sister', 'string']
+          ]
+        })
+      } catch (err) {
+        expect(err.code).to.equal('EACCES')
+        return
+      }
+
+      assert.fail('Did not throw')
+    })
+
+    it('empty header map', async () => {
+      try {
+        await xmlFlatten2csv({
+          xmlPath: sourceFile,
+          csvPath: outputFile,
+          rootXMLElement: 'Episode',
+          pivotPath: '$.Person',
+          headerMap: []
+        })
+      } catch (err) {
+        return
+      }
+
+      assert.fail('Did not throw')
+    })
+  })
+
 })
