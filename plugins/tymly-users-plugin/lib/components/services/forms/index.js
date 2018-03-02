5e2418ffdac40988d3532d57905a4e3ba11b8703
meena.hoda <meena.hoda@wmfs.net>
2018-03-02 12:44:17 +0000

    Form service only adds to forms when it's not a yml and has a jsonSchema

--- plugins/tymly-users-plugin/lib/components/services/forms/index.js
+++ plugins/tymly-users-plugin/lib/components/services/forms/index.js
@@ -21,24 +21,37 @@ class FormsService {
         if (!Object.keys(formDefinitions).includes(formId)) {
           const meta = {
             yamlPath: formDef.filePath,
-            namespace: formDef.namespace
+            namespace: _.camelCase(formDef.namespace)
           }
-          meta.formName = path.basename(meta.yamlPath, '.yml')
-          meta.modelName = path.basename(meta.yamlPath, '.yml')
+          meta.formName = _.camelCase(path.basename(meta.yamlPath, '.yml'))
+          meta.modelName = _.camelCase(path.basename(meta.yamlPath, '.yml'))
 
-          const blueprintPath = meta.yamlPath.split(path.join('forms', meta.formName + '.yml'))[0]
+          const blueprintPath = meta.yamlPath.split(path.join('forms', _.kebabCase(meta.formName) + '.yml'))[0]
           if (!fs.existsSync(path.resolve(blueprintPath, 'state-machines'))) fs.mkdirSync(path.resolve(blueprintPath, 'state-machines'))
           if (!fs.existsSync(path.resolve(blueprintPath, 'models'))) fs.mkdirSync(path.resolve(blueprintPath, 'models'))
 
           promises.push(this.writeBlueprintFiles(options, meta, blueprintPath))
         }
+      } else if (!this.forms[formId]) {
+        formDef.shasum = shasum(formDef)
+        this.forms[formId] = formDef
       }
-      formDef.shasum = shasum(formDef)
-      this.forms[formId] = formDef
     })
 
     Promise.all(promises)
-      .then(() => callback(null))
+      .then(results => {
+        results.map(res => {
+          const formDef = {
+            jsonSchema: res.jsonSchema,
+            namespace: Object.keys(res)[0].split('_')[0],
+            id: Object.keys(res)[0].split('_')[1],
+            name: Object.keys(res)[0].split('_')[1]
+          }
+          formDef.shasum = shasum(formDef)
+          this.forms[Object.keys(res)[0]] = formDef
+        })
+        callback(null)
+      })
       .catch(err => callback(err))
   }
 
@@ -47,28 +60,28 @@ class FormsService {
     return new Promise((resolve, reject) => {
       formMaker(meta, (err, result) => {
         if (err) return reject(err)
-        jsonfile.writeFile(path.resolve(blueprintPath, 'forms', meta.formName + '.json'), result.form, {
+        jsonfile.writeFile(path.resolve(blueprintPath, 'forms', _.kebabCase(meta.formName) + '.json'), result.form, {
           spaces: 2,
           EOL: '\n'
         }, err => {
           if (err) return reject(err)
-          jsonfile.writeFile(path.resolve(blueprintPath, 'state-machines', meta.formName + '.json'), result.stateMachine, {
+          jsonfile.writeFile(path.resolve(blueprintPath, 'state-machines', _.kebabCase(meta.formName) + '.json'), result.stateMachine, {
             spaces: 2,
             EOL: '\n'
           }, err => {
             if (err) return reject(err)
             if (!options.blueprintComponents.models[`${meta.namespace}_${meta.modelName}`]) {
-              jsonfile.writeFile(path.resolve(blueprintPath, 'models', meta.modelName + '.json'), result.model, {
+              jsonfile.writeFile(path.resolve(blueprintPath, 'models', _.kebabCase(meta.modelName) + '.json'), result.model, {
                 spaces: 2,
                 EOL: '\n'
               }, err => {
                 if (err) return reject(err)
                 options.blueprintComponents.models[`${meta.namespace}_${meta.modelName}`] = getModelDefinition(result.model, meta)
                 options.blueprintComponents.stateMachines[`${meta.namespace}_${meta.formName}_1_0`] = getStateMachineDefinition(result.stateMachine, meta)
-                resolve()
+                resolve({[`${meta.namespace}_${meta.formName}`]: result.form})
               })
             } else {
-              resolve()
+              resolve({[`${meta.namespace}_${meta.formName}`]: result.form})
             }
           })
         })
