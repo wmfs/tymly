f4f9c656071efd8d55b42736fc1cf2573df06f71
Meena.Hoda <meena.hoda@wmfs.net>
2018-03-05 09:41:35 +0000

    Refresh ranking state resource also re-generates the stats as well as the view

--- plugins/tymly-rankings-plugin/lib/components/state-resources/refresh-ranking/index.js
+++ plugins/tymly-rankings-plugin/lib/components/state-resources/refresh-ranking/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const generateView = require('./../../services/rankings/generate-view-statement.js')
+const generateStats = require('./../../services/rankings/generate-stats.js')
 
 class RefreshRanking {
   init (resourceConfig, env, callback) {
     this.rankings = env.blueprintComponents.rankings
     this.client = env.bootedServices.storage.client
     this.registry = env.bootedServices.registry
+    this.storage = env.bootedServices.storage
     callback(null)
   }
 
@@ -14,6 +16,7 @@ class RefreshRanking {
     const schema = event.schema
     const category = event.category
     const key = schema + '_' + category
+
     const statement = generateView({
       category: category,
       schema: schema,
@@ -24,9 +27,24 @@ class RefreshRanking {
       }
     })
 
-    await this.client.query(statement)
-      .then(() => context.sendTaskSuccess())
-      .catch(err => context.sendTaskFailure({error: 'generateViewFail', cause: err}))
+    const rankingModel = this.storage.models[`${schema}_${this.rankings[key].rankingModel}`]
+    const statsModel = this.storage.models[`${schema}_${this.rankings[key].statsModel}`]
+
+    generateStats({
+      client: this.client,
+      category: category,
+      schema: schema,
+      pk: this.rankings[key].source.property,
+      name: schema,
+      rankingModel: rankingModel,
+      statsModel: statsModel,
+      registry: this.registry.registry[key]
+    }, async (err) => {
+      if (err) return context.sendTaskFailure({error: 'generateStatsFail', cause: err})
+      await this.client.query(statement)
+        .then(() => context.sendTaskSuccess())
+        .catch(err => context.sendTaskFailure({error: 'generateViewFail', cause: err}))
+    })
   }
 }
 
