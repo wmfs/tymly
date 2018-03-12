609627671df1556a389e4f14808fbce0bf2686ee
Jez Higgins <jez@jezuk.co.uk>
2018-03-12 14:57:05 +0000

    Through an exception if the function isn't found, rather than trying to deref an undefined

--- packages/tymly/lib/getFunction.js
+++ packages/tymly/lib/getFunction.js
@@ -2,6 +2,9 @@
 
 module.exports = function getFunction (options, funcName) {
   const services = options.services || options.bootedServices
-
-  return services.functions.functions[funcName].func
+  const func = services.functions.functions[funcName]
+  if (!func) {
+    throw new Error(`Function '${funcName}' not registered`)
+  }
+  return func.func
 }
