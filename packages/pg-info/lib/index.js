6b4aa9f1c022158fcbe4925d4859cec08c3913e2
Jez Higgins <jez@jezuk.co.uk>
2018-03-27 15:23:01 +0100

    incremental refactor

--- packages/pg-info/lib/index.js
+++ packages/pg-info/lib/index.js
@@ -28,142 +28,132 @@ async function pgInfo (options) {
   // const pgViews = queryResults[8]
 
   // Loop over each requested schema namen
-  schemas.forEach(
-    function (requestedSchemaName) {
-      pgSchemas.forEach(
-        function (candidatePgSchema) {
-          if (candidatePgSchema.schema_name === requestedSchemaName) {
-            // Database has this schema name
-            info.schemas[requestedSchemaName] = {
-              schemaExistsInDatabase: true,
-              comment: candidatePgSchema.schema_comment,
-              tables: {}
+  for (const requestedSchemaName of schemas) {
+    const candidatePgSchema = pgSchemas.find(s => s.schema_name === requestedSchemaName)
+
+    if (!candidatePgSchema) {
+      // Database doesn't have this schema name
+      info.schemas[requestedSchemaName] = {
+        schemaExistsInDatabase: false
+      }
+      continue;
+    }
+
+    // Database has this schema name
+    info.schemas[requestedSchemaName] = {
+      schemaExistsInDatabase: true,
+      comment: candidatePgSchema.schema_comment,
+      tables: {}
+    }
+
+
+    const schemaTables = pgTables.filter(table => table.table_schema === requestedSchemaName)
+    for (const candidatePgTable of schemaTables) {
+      const matchSchemaAndTable = o => o.table_schema === requestedSchemaName && o.table_name === candidatePgTable.table_name
+
+      const pkColumnNames = pgPkColumns
+        .filter(matchSchemaAndTable)
+        .map(col => col.pk_column_name)
+
+      const columns = {}
+      pgColumns
+        .filter(matchSchemaAndTable)
+        .forEach(col => {
+          const columnInfo = {
+            columnDefault: col.column_default,
+            isNullable: col.is_nullable,
+            dataType: col.data_type,
+            characterMaximumLength: col.character_maximum_length,
+            numericScale: col.numeric_scale,
+            comment: col.column_comment
+          }
+          if (columnInfo.dataType === 'ARRAY') {
+            columnInfo.array = true
+            columnInfo.dataType = col.array_element_type.slice(1)
+          } else {
+            columnInfo.array = false
+          }
+          columns[col.column_name] = columnInfo
+        }
+      )
+
+      const indexes = {}
+      const tableIndexes = _.filter(pgIndexes, {table_name: requestedSchemaName + '.' + candidatePgTable.table_name})
+      tableIndexes.forEach(
+        function (tableIndex) {
+          if (!tableIndex.is_primary) {
+            indexes[tableIndex.index_name] = {
+              columns: [
+                tableIndex.index_keys
+              ],
+              unique: tableIndex.is_unique,
+              method: tableIndex.method
             }
-
-            pgTables.forEach(
-              function (candidatePgTable) {
-                if (candidatePgTable.table_schema === requestedSchemaName) {
-                  const pkColumnNames = []
-                  pgPkColumns.forEach(
-                    function (candidatePkColumn) {
-                      if (candidatePkColumn.table_schema === requestedSchemaName && candidatePkColumn.table_name === candidatePgTable.table_name) {
-                        pkColumnNames.push(candidatePkColumn.pk_column_name)
-                      }
-                    }
-                  )
-
-                  const columns = {}
-                  pgColumns.forEach(
-                    function (candidateColumn) {
-                      if (candidateColumn.table_schema === requestedSchemaName && candidateColumn.table_name === candidatePgTable.table_name) {
-                        const columnInfo = {
-                          columnDefault: candidateColumn.column_default,
-                          isNullable: candidateColumn.is_nullable,
-                          dataType: candidateColumn.data_type,
-                          characterMaximumLength: candidateColumn.character_maximum_length,
-                          numericScale: candidateColumn.numeric_scale,
-                          comment: candidateColumn.column_comment
-                        }
-                        if (columnInfo.dataType === 'ARRAY') {
-                          columnInfo.array = true
-                          columnInfo.dataType = candidateColumn.array_element_type.slice(1)
-                        } else {
-                          columnInfo.array = false
-                        }
-                        columns[candidateColumn.column_name] = columnInfo
-                      }
-                    }
-                  )
-
-                  const indexes = {}
-                  const tableIndexes = _.filter(pgIndexes, {table_name: requestedSchemaName + '.' + candidatePgTable.table_name})
-                  tableIndexes.forEach(
-                    function (tableIndex) {
-                      if (!tableIndex.is_primary) {
-                        indexes[tableIndex.index_name] = {
-                          columns: [
-                            tableIndex.index_keys
-                          ],
-                          unique: tableIndex.is_unique,
-                          method: tableIndex.method
-                        }
-                      }
-                    }
-                  )
-
-                  const triggers = {}
-                  const tableTriggers = _.filter(pgTriggers, {
-                    trigger_schema: requestedSchemaName,
-                    event_object_table: candidatePgTable.table_name
-                  })
-                  tableTriggers.forEach(
-                    function (tableTrigger) {
-                      triggers[tableTrigger.trigger_name] = {
-                        eventManipulation: tableTrigger.event_manipulation,
-                        actionCondition: tableTrigger.action_condition,
-                        actionStatement: tableTrigger.action_statement,
-                        actionOrientation: tableTrigger.action_orientation,
-                        actionTiming: tableTrigger.action_timing
-                      }
-                    }
-                  )
-
-                  const functions = {}
-                  const tableFunctions = _.filter(pgFunctions, {specific_schema: requestedSchemaName})
-                  tableFunctions.forEach(
-                    function (tableFunction) {
-                      functions[tableFunction.routine_name] = {
-                        dataType: tableFunction.data_type
-                      }
-                    }
-                  )
-
-                  const fkConstraints = {}
-                  const tableFkConstraints = _.filter(pgFkConstraints, {source_table: requestedSchemaName + '.' + candidatePgTable.table_name})
-                  tableFkConstraints.forEach(
-                    function (fkConstraint) {
-                      const fkName = fkConstraint.constraint_name
-                      if (!fkConstraints.hasOwnProperty(fkName)) {
-                        fkConstraints[fkName] = {
-                          targetTable: fkConstraint.target_table,
-                          sourceColumns: [fkConstraint.source_column],
-                          targetColumns: [fkConstraint.target_column],
-                          updateAction: FK_ACTION_CODES[fkConstraint.update_action],
-                          deleteAction: FK_ACTION_CODES[fkConstraint.delete_action],
-                          matchType: FK_MATCH_TYPES[fkConstraint.match_type]
-                        }
-                      } else {
-                        fkConstraints[fkName].sourceColumns.push(fkConstraint.source_column)
-                        fkConstraints[fkName].targetColumns.push(fkConstraint.target_column)
-                      }
-                    }
-                  )
-
-                  // This table is this schema
-                  const tables = info.schemas[requestedSchemaName].tables
-                  tables[candidatePgTable.table_name] = {
-                    comment: candidatePgTable.table_comment,
-                    pkColumnNames: pkColumnNames,
-                    columns: columns,
-                    indexes: indexes,
-                    triggers: triggers,
-                    functions: functions,
-                    fkConstraints: fkConstraints
-                  }
-                }
-              }
-            )
           }
         }
       )
-      if (!info.schemas.hasOwnProperty(requestedSchemaName)) {
-        // Database doesn't have this schema name
-        info.schemas[requestedSchemaName] = {
-          schemaExistsInDatabase: false
+
+      const triggers = {}
+      const tableTriggers = _.filter(pgTriggers, {
+        trigger_schema: requestedSchemaName,
+        event_object_table: candidatePgTable.table_name
+      })
+      tableTriggers.forEach(
+        function (tableTrigger) {
+          triggers[tableTrigger.trigger_name] = {
+            eventManipulation: tableTrigger.event_manipulation,
+            actionCondition: tableTrigger.action_condition,
+            actionStatement: tableTrigger.action_statement,
+            actionOrientation: tableTrigger.action_orientation,
+            actionTiming: tableTrigger.action_timing
+          }
         }
+      )
+
+      const functions = {}
+      const tableFunctions = _.filter(pgFunctions, {specific_schema: requestedSchemaName})
+      tableFunctions.forEach(
+        function (tableFunction) {
+          functions[tableFunction.routine_name] = {
+            dataType: tableFunction.data_type
+          }
+        }
+      )
+
+      const fkConstraints = {}
+      const tableFkConstraints = _.filter(pgFkConstraints, {source_table: requestedSchemaName + '.' + candidatePgTable.table_name})
+      tableFkConstraints.forEach(
+        function (fkConstraint) {
+          const fkName = fkConstraint.constraint_name
+          if (!fkConstraints.hasOwnProperty(fkName)) {
+            fkConstraints[fkName] = {
+              targetTable: fkConstraint.target_table,
+              sourceColumns: [fkConstraint.source_column],
+              targetColumns: [fkConstraint.target_column],
+              updateAction: FK_ACTION_CODES[fkConstraint.update_action],
+              deleteAction: FK_ACTION_CODES[fkConstraint.delete_action],
+              matchType: FK_MATCH_TYPES[fkConstraint.match_type]
+            }
+          } else {
+            fkConstraints[fkName].sourceColumns.push(fkConstraint.source_column)
+            fkConstraints[fkName].targetColumns.push(fkConstraint.target_column)
+          }
+        }
+      )
+
+      // This table is this schema
+      const tables = info.schemas[requestedSchemaName].tables
+      tables[candidatePgTable.table_name] = {
+        comment: candidatePgTable.table_comment,
+        pkColumnNames: pkColumnNames,
+        columns: columns,
+        indexes: indexes,
+        triggers: triggers,
+        functions: functions,
+        fkConstraints: fkConstraints
       }
     }
-  )
+  }
 
   return info
 }
