3de89b23581516ce6a2f676b03787f00cf15622b
Jez Higgins <jez@jezuk.co.uk>
2018-03-28 12:59:21 +0100

    alter view

--- packages/pg-diff-sync/lib/statement-generators/view/index.js
+++ packages/pg-diff-sync/lib/statement-generators/view/index.js
@@ -1,5 +1,5 @@
 function viewStatementGenerator (viewId, view, statements) {
-  if (!view.base) {
+  if ((!view.base) || (view.base.sql !== view.target.sql)) {
     statements.push(`CREATE OR REPLACE VIEW ${viewId} AS ${view.target.sql}`)
   }
 }
