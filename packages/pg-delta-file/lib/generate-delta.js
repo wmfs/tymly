c69ec6eca965b7e24582f6ff91a2894d335ed8ec
Jez Higgins <jez@jezuk.co.uk>
2018-04-05 09:58:21 +0100

    Clarifying refactors - switch from async.eachSeries to a for loop with await.

--- packages/pg-delta-file/lib/generate-delta.js
+++ packages/pg-delta-file/lib/generate-delta.js
@@ -1,42 +1,37 @@
-'use strict'
-
-const async = require('async')
 const fs = require('fs')
 const QueryStream = require('pg-query-stream')
 const Transformer = require('./Transformer')
 
-module.exports = function generateDelta (options, callback) {
+module.exports = async function generateDelta (options, callback) {
   const deltaFileWriteStream = fs.createWriteStream(options.outputFilepath, {defaultEncoding: 'utf8'})
   const info = {
     totalCount: 0
   }
-  async.eachSeries(
-    Object.keys(options.csvExtracts),
-    (model, cb) => {
-      const modified = options.modifiedColumnName || '_modified'
 
-      const table = tableName(options.namespace, model)
-
-      const sql = `select * from ${table} where ${modified} >= $1`
-      const csvTransform = (sql, values, client) => {
-        const dbStream = client.query(new QueryStream(sql, values))
-        dbStream.on('end', () => cb(null))
-
-        const csvTransformer = new Transformer(info, model, options)
-        dbStream.pipe(csvTransformer).pipe(deltaFileWriteStream, {end: false})
-      }
-      options.client.run([{sql: sql, params: [options.since], action: csvTransform}])
-    },
-    (err) => {
-      deltaFileWriteStream.end()
-      if (err) {
-        callback(err)
-      } else {
-        callback(null, info)
-      }
+  try {
+    for (const model of Object.keys(options.csvExtracts)) {
+      await extractModel(info, model, options, deltaFileWriteStream)
     }
-  )
-}
+  } finally {
+    deltaFileWriteStream.end()
+  }
+
+  return info
+} // generateDelta
+
+function extractModel (info, model, options, outStream) {
+  const modified = options.modifiedColumnName || '_modified'
+
+  const table = tableName(options.namespace, model)
+
+  const sql = `select * from ${table} where ${modified} >= $1`
+  const csvTransform = (sql, values, client) => {
+    const dbStream = client.query(new QueryStream(sql, values))
+    const csvTransformer = new Transformer(info, model, options)
+    dbStream.pipe(csvTransformer).pipe(outStream, {end: false})
+  }
+  return options.client.run([{sql: sql, params: [options.since], action: csvTransform}])
+} // extractModel
 
 function tableName (namespace, model) {
   const separator = model.indexOf('.')
--- packages/pg-delta-file/lib/index.js
+++ packages/pg-delta-file/lib/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const makeDir = require('make-dir')
 const getDirName = require('path').dirname
 const generateDelta = require('./generate-delta')
@@ -8,10 +6,7 @@ module.exports = function setup (options, callback) {
   const outputDirPath = getDirName(options.outputFilepath)
 
   makeDir(outputDirPath)
-    .then(function () {
-      generateDelta(options, callback)
-    })
-    .catch(function (err) {
-      callback(err)
-    })
+    .then(() => generateDelta(options))
+    .then(info => callback(null, info))
+    .catch(err => callback(err))
 }
