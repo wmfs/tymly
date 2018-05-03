e3719539cfe9839d76f068b6dfc923eb10f4a80f
Meena.Hoda <meena.hoda@wmfs.net>
2018-05-03 13:20:23 +0100

    Added custom header to allowed headers

--- plugins/tymly-express-plugin/lib/components/services/server/index.js
+++ plugins/tymly-express-plugin/lib/components/services/server/index.js
@@ -29,7 +29,7 @@ class ExpressServerService {
       function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Custom-Header')
         next()
       }
     )
