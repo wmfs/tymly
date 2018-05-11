5583ebe131cf1fe7d906cf0ee69b6cce89842c44
Meena.Hoda <meena.hoda@wmfs.net>
2018-05-11 09:57:05 +0100

    implementation of sending mail state resource

--- plugins/tymly-gov-uk-notify-plugin/lib/components/state-resources/sending-mail-via-service/index.js
+++ plugins/tymly-gov-uk-notify-plugin/lib/components/state-resources/sending-mail-via-service/index.js
@@ -1,11 +1,33 @@
 'use strict'
 
+const NotifyClient = require('notifications-node-client').NotifyClient
+
 module.exports = class SendingMailViaService {
   init (stateConfig, options, callback) {
+    this.notifyClient = process.env.GOV_UK_NOTIFY_API_KEY && new NotifyClient(process.env.GOV_UK_NOTIFY_API_KEY)
+    this.templateId = stateConfig.templateId
+    if (process.env.PROXY_URL) this.notifyClient.setProxy(process.env.PROXY_URL)
     callback(null)
   }
 
   run (event, context) {
-    context.sendTaskSuccess()
+    if (this.notifyClient) {
+      this.notifyClient
+        .sendEmail(
+          this.templateId,
+          event.emailAddress,
+          {}
+        )
+        .then(response => {
+          if (response.statusCode === 201) context.sendTaskSuccess()
+          else context.sendTaskFailure({cause: new Error('created mail fail'), error: 'Failed to Create Mail'})
+        })
+        .catch(err => context.sendTaskFailure({cause: err, error: 'Send Mail Fail'}))
+    } else {
+      context.sendTaskFailure({
+        cause: new Error('missing env variable'),
+        error: 'Missing ENV: GOV_UK_NOTIFY_API_KEY'
+      })
+    }
   }
 }
---
+++ plugins/tymly-gov-uk-notify-plugin/test/send-mail-spec.js
@@ -0,0 +1,60 @@
+/* eslint-env mocha */
+
+const expect = require('chai').expect
+const tymly = require('tymly')
+const path = require('path')
+const process = require('process')
+
+const SEND_MAIL_STATE_MACHINE_NAME = 'test_sendWelcomeMail'
+
+describe('Send Mail tests', function () {
+  this.timeout(process.env.TIMEOUT || 5000)
+
+  let tymlyService, statebox
+
+  it('boot tymly', done => {
+    tymly.boot(
+      {
+        pluginPaths: [
+          path.resolve(__dirname, './../lib')
+        ],
+        blueprintPaths: [
+          path.resolve(__dirname, './fixtures/blueprints/welcome-blueprint')
+        ],
+        config: {}
+      },
+      (err, tymlyServices) => {
+        expect(err).to.eql(null)
+        tymlyService = tymlyServices.tymly
+        statebox = tymlyServices.statebox
+        done()
+      }
+    )
+  })
+
+  it('start state machine to send mail with an email expected to succeed', done => {
+    statebox.startExecution(
+      {
+        emailAddress: 'simulate-delivered@notifications.service.gov.uk'
+      },
+      SEND_MAIL_STATE_MACHINE_NAME,
+      {
+        sendResponse: 'COMPLETE'
+      },
+      (err, executionDescription) => {
+        if (process.env.GOV_UK_NOTIFY_API_KEY) {
+          expect(err).to.eql(null)
+          expect(executionDescription.status).to.eql('SUCCEEDED')
+        } else {
+          expect(executionDescription.status).to.eql('FAILED')
+          expect(executionDescription.errorCode).to.eql('Missing ENV: GOV_UK_NOTIFY_API_KEY')
+        }
+        done()
+      }
+    )
+  })
+
+  it('should shutdown Tymly', async () => {
+    await tymlyService.shutdown()
+  })
+})
