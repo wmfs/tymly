c334ac3b53dcef8093643374fc1993a0cf213b54
Jez Higgins <jez@jezuk.co.uk>
2018-05-18 14:33:14 +0100

    docs: replaced reference to MongoDb with truthful reference to Postgresql :D
    affects: tymly-pg-plugin

--- plugins/tymly-pg-plugin/lib/components/services/storage/doc/index.js
+++ plugins/tymly-pg-plugin/lib/components/services/storage/doc/index.js
@@ -1,8 +1,7 @@
 'use strict'
 
 module.exports = {
-  description: 'Replaces the default in-memory storage solution with a MongoDB-backed alternative',
-  example: require('./example.json'),
+  description: 'Replaces the default in-memory storage solution with a Postgresql-backed alternative',
   blueprintDirs: {
     models: "One JSON file per model (contents to be a JSON schema for defining the model's data structure)"
   }
