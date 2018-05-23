fd47096ab1f22d6cc53013fec13fc8cdda26bf39
Meena.Hoda <meena.hoda@wmfs.net>
2018-05-23 10:17:03 +0100

    get notifications to get only un-acknowledged notifications

--- plugins/tymly-users-plugin/lib/components/state-resources/get-notifications/index.js
+++ plugins/tymly-users-plugin/lib/components/state-resources/get-notifications/index.js
@@ -20,16 +20,19 @@ class GetNotifications {
     }
     if (event.startFrom) findOptions.where.created = {moreThanEquals: new Date(event.startFrom).toLocaleString()}
 
-    this.notifications.find(findOptions)
+    this.notifications
+      .find(findOptions)
       .then(results => {
-        results.map(r => {
-          payload.notifications.push({
-            id: r.id,
-            title: r.title,
-            description: r.description,
-            category: r.category,
-            created: r.created
-          })
+        results.forEach(r => {
+          if (!r.acknowledged) {
+            payload.notifications.push({
+              id: r.id,
+              title: r.title,
+              description: r.description,
+              category: r.category,
+              created: r.created
+            })
+          }
         })
         payload.totalNotifications = payload.notifications.length
         payload.limit = event.limit || 10
