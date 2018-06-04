b5dc6bf6b0223f30376b55db6af1eb114ce83d39
Reece Brend <reece.brend@wmfs.net>
2018-06-04 11:31:36 +0100

    Cast more things

--- plugins/tymly-rankings-plugin/lib/components/services/rankings/case-statements/option.js
+++ plugins/tymly-rankings-plugin/lib/components/services/rankings/case-statements/option.js
@@ -10,17 +10,21 @@ module.exports = function options (factorName, factorObj, schema, table, column)
       statement += `WHEN upper(${table}.${column}) = upper('${i.textualValue}') THEN ${i.score} `
       cast = `::int`
     } else if (i.type === 'numeric-constant') {
+      cast = `::int`
       statement += `WHEN ${table}.${column}${cast} = ${i.numericValue} THEN ${i.score} `
     } else if (i.type === 'numeric-range') {
+      cast = `::int`
       statement += `WHEN ${table}.${column}${cast} BETWEEN ${i.minimum} AND ${i.maximum} THEN ${i.score} `
     } else if (i.type === 'numeric-boundary') {
+      cast = `::int`
       if (i.operator === 'greaterThan') {
         statement += `WHEN ${table}.${column}${cast} > ${i.numericValue} THEN ${i.score} `
       } else if (i.operator === 'lessThan') {
         statement += `WHEN ${table}.${column}${cast} < ${i.numericValue} THEN ${i.score} `
       }
     } else if (i.type === 'boolean-equals') {
-      statement += `WHEN ${table}.${column} = ${i.booleanValue} THEN ${i.score} `
+      cast = `::BOOLEAN`
+      statement += `WHEN ${table}.${column}${cast} = ${i.booleanValue} THEN ${i.score} `
     }
   })
   statement += `ELSE ${factorObj.default} END AS ${_.snakeCase(factorName)}_score`
